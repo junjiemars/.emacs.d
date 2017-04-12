cab663b6bdf9ae2740182ac5b6f2b09c94ec081e
南山竹 <junjiemars@gmail.com>
2017-04-12 23:55:13 +0800

    lexical-binding in `take' and 'int-to-binary-string'

--- private/utils.el
+++ private/utils.el
@@ -6,11 +6,12 @@
 (defun take (n seq)
   "Returns a sequence of the first n itmes in seq, or all items if
    there are fewer than n."
-  (let ((acc nil) (n1 n) (s1 seq))
-    (while (and (> n1 0) s1)
-      (setq acc (cons (car s1) acc))
-      (setq n1 (1- n1) s1 (cdr s1)))
-    (nreverse acc)))
+  (let ((lexical-binding t))
+    (let ((acc nil) (n1 n) (s1 seq))
+      (while (and (> n1 0) s1)
+        (setq acc (cons (car s1) acc))
+        (setq n1 (1- n1) s1 (cdr s1)))
+      (nreverse acc))))
 
 (safe-do-when number-sequence (fset 'range 'number-sequence))
 
@@ -21,11 +22,12 @@
 
 (defun int-to-binary-string (i)
   "Display an integer in binary string representation."
-  (let ((s ""))
-    (while (not (= i 0))
-      (setq s (concat (if (= 1 (logand i 1)) "1" "0") s))
-      (setq i (lsh i -1)))
-    (concat "#b" (if (string= s "") (setq s "0") s))))
+  (let ((lexical-binding t))
+    (let ((s ""))
+      (while (not (= i 0))
+        (setq s (concat (if (= 1 (logand i 1)) "1" "0") s))
+        (setq i (lsh i -1)))
+      (concat "#b" (if (string= s "") (setq s "0") s)))))
 
 
 (defun clone-themes ()
