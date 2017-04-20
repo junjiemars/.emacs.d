6c5c45f12bddf5d52b54cff4b9281c63f2a6d275
南山竹 <junjiemars@gmail.com>
2017-04-20 16:54:21 +0800

    useless `gud-lldb-patch.el', remove it

--- config/debugger.el
+++ config/debugger.el
@@ -2,15 +2,4 @@
 ;; Debugger setup script
 ;;
 
-;; lldb on darwin
-(platform-supported-p
- darwin
- (compile-and-load-elisp-files
-  '("gud-lldb-patch.el") "config/"))
 
-;; lldb on linux
-(platform-supported-p
- gnu/linux
- (when (bin-exists-p "lldb")
-   (compile-and-load-elisp-files
-    '("gud-lldb-patch.el") "config/")))
