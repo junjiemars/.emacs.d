1eb74bcc2f920427381c98434837d36b51db91b0
南山竹 <junjiemars@gmail.com>
2017-04-22 13:46:38 +0800

    move the slow parts in setup-navigation.el into after-init-hook

--- config/setup-navigation.el
+++ config/setup-navigation.el
@@ -5,25 +5,29 @@
 
 ;; These customizations make it easier for you to navigate files,
 ;; switch buffers, and choose options from the minibuffer.
+;; But it's loading slow, so load it after-init
+
+(add-hook
+ 'after-init-hook
+ (lambda ()
+   ;; This enables ido in all contexts where it could be useful, not just
+   ;; for selecting buffer and file names
+   (ido-ubiquitous-mode 1)
+
+   ;; Enhances M-x to allow easier execution of commands. Provides
+   ;; a filterable list of possible commands in the minibuffer
+   ;; http://www.emacswiki.org/emacs/Smex
+   ;; https://github.com/nonsequitur/smex
+   (global-set-key (kbd "M-x")
+                   (lambda ()
+                     (interactive)
+                     (or (boundp 'smex-cache)
+                         (smex-initialize))
+                     (smex)))
+   
+   (safe-setq smex-save-file (concat user-emacs-directory ".smex-items"))))
 
 
-
-;; This enables ido in all contexts where it could be useful, not just
-;; for selecting buffer and file names
-(ido-ubiquitous-mode 1)
-
-
-;; Enhances M-x to allow easier execution of commands. Provides
-;; a filterable list of possible commands in the minibuffer
-;; http://www.emacswiki.org/emacs/Smex
-;; https://github.com/nonsequitur/smex
-(global-set-key (kbd "M-x")
-                (lambda ()
-                  (interactive)
-                  (or (boundp 'smex-cache)
-                      (smex-initialize))
-                  (smex)))
-(safe-setq smex-save-file (concat user-emacs-directory ".smex-items"))
 
 
 
