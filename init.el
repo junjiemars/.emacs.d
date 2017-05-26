6b57bdaa538f1fd2e2dea17e89c699cf24c22778
南山竹 <junjiemars@gmail.com>
2017-05-26 10:14:12 +0800

    remove custom-set-variables in init.el

--- init.el
+++ init.el
@@ -481,17 +481,3 @@ self things.
 
 ;; ^ End of init.el
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-    (tagedit sx smex slime rainbow-delimiters paredit magit inf-clojure ido-ubiquitous geiser dockerfile-mode docker-tramp clojure-mode-extra-font-locking cider cdlatex bing-dict auctex aggressive-indent))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
