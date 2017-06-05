569a38bddec5dbb2336690717378d1dde352fe0c
南山竹 <junjiemars@gmail.com>
2017-06-05 22:16:09 +0800

    clean init.el, why automatically add `defcustom' staff

--- init.el
+++ init.el
@@ -447,17 +447,3 @@ self things.
 ;; ^ End of init.el
 
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-    (aggressive-indent bing-dict ido-ubiquitous markdown-mode paredit rainbow-delimiters smex tagedit auctex cdlatex cider clojure-mode clojure-mode-extra-font-locking inf-clojure dockerfile-mode docker-tramp ereader magit geiser slime sx))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
