c0023e6f2e4efcefe81c426ecd39d2d7cc41eeeb
南山竹 <junjiemars@gmail.com>
2017-06-06 10:11:54 +0800

    eshell-command-result more slower than shell-command #18 #20

--- init.el
+++ init.el
@@ -5,13 +5,6 @@
 
 
 
-
-;; Added by Package.el.  This must come before configurations of
-;; installed packages.  Don't delete this line.  If you don't want it,
-;; just comment it out by adding a semicolon to the start of the line.
-;; You may delete these explanatory comments.
-(package-initialize)
-
 (defmacro comment (&rest body)
   "Ignores body, yields nil."
   nil)
@@ -173,19 +166,6 @@ If in terminal mode, and there are no ELSE’s, the value is nil.
     (let ((_b_ (concat "hash " `,b " &>/dev/null")))
       `(zerop (shell-command ,_b_)))))
 
-(defmacro bin-exists-p0 (b)
-  "Returns true if BIN-NAME exists in env.
-
-\(fn BIN-NAME)"
-  (let ((_b_ (concat "which " `,b)))
-    (eshell-command-result `,_b_ '_bin_exists_p_)
-    `(zerop ,_bin_exists_p_)))
-
-
-(defmacro shell-command->str-no-newline (c)
-  (let ((_s_ (shell-command-to-string `,c)))
-    `(replace-regexp-in-string "\n$" "" ,_s_)))
-
 
 (defmacro trim-right-newline (s)
   `(replace-regexp-in-string "\n$" "" ,s))
@@ -197,19 +177,9 @@ If in terminal mode, and there are no ELSE’s, the value is nil.
 \(fn BIN-NAME)"
   (platform-supported-if windows-nt
       (let ((_b_ (concat "where " `,b)))
-        `(shell-command->str-no-newline ,_b_))
+        `(trim-right-newline (shell-command-to-string ,_b_)))
     (let ((_b_ (concat "type -P " `,b)))
-      `(shell-command->str-no-newline ,_b_))))
-
-
-(defmacro bin-path0 (b)
-  "Returns the path of BIN-NAME in env.
-
-\(fn BIN-NAME)"
-  (let ((_p_ (eshell-command-result (concat "which " `,b)
-                                    '_bin_path_))) 
-    (when (zerop _bin_path_)
-      (trim-right-newline `,_p_))))
+      `(trim-right-newline (shell-command-to-string ,_b_)))))
 
 
 (defmacro safe-call (fn &rest args)
