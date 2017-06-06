8bac4200de7c282b7d23d244162d509d9c0a2952
南山竹 <junjiemars@gmail.com>
2017-06-06 16:24:16 +0800

    improve shell.el on Windows #23
    1. common shell env for all platforms;
    2. clean windows shell env;

--- config/shell.el
+++ config/shell.el
@@ -16,99 +16,93 @@
        (kill-buffer sexpr-buffer))))
 
 
-;; Non-Windows OS shell env
-(platform-supported-unless
-    windows-nt
+(defmacro set-default-shell! (path regexp)
+  "Set default SHELL"
+  `(progn
+     ,(when (or (null (getenv "SHELL"))
+                (not (string-match regexp (getenv "SHELL"))))
+        `(setenv "SHELL" (file-name-base ,path)))
+     ,(when (or (null shell-file-name)
+                (not (string-match regexp shell-file-name)))
+        `(setq shell-file-name ,path))))
 
   
-  (defmacro set-default-shell! (shell rshell)
-    "Set default SHELL"
-    `(progn
-       ,(when (or (null (getenv "SHELL"))
-                  (not (string-match rshell (getenv "SHELL"))))
-          `(setenv "SHELL" ,shell))
-       ,(when (or (null shell-file-name)
-                  (not (string-match rshell shell-file-name)))
-          `(setq shell-file-name ,shell))))
+(defmacro set-path-env! ()
+  "Set PATH and exec-path in Emacs."
+  `(let* ((p (shell-command-to-string
+              "$SHELL -i -c 'echo -n $PATH' 2>/dev/null"))
+          (x (split-string p ":")))
+     (setenv "PATH" p)
+     (while (car x)
+       (add-to-list 'exec-path (car x) t #'string=)
+       (setq x (cdr x)))))
+
+
+(defmacro path-env ()
+  "Return the `cons' of virtualized `path-env' source and compiled file name."
+  (let ((_v_ (make-vdir "config/")))
+    `(cons ,(concat _v_ ".path-env.el")
+           ,(concat _v_ ".path-env.elc"))))
 
   
-  (defmacro set-path-env! ()
-    "Set PATH and exec-path in Emacs."
-    `(let* ((p (shell-command-to-string
-                "$SHELL -i -c 'echo -n $PATH' 2>/dev/null"))
-            (x (split-string p ":")))
-       (setenv "PATH" p)
-       (while (car x)
-         (add-to-list 'exec-path (car x) t #'string=)
-         (setq x (cdr x)))))
+(defun save-path-env ()
+  (let ((env (path-env)))
+    (set-path-env!)
+    (save-sexpr-to-file
+     (list 'progn
+           (list 'setenv "PATH" (getenv "PATH"))
+           (list 'setq 'exec-path (list 'quote exec-path)))
+     (car env))
+    (byte-compile-file (car env))))
 
 
-  (defmacro path-env ()
-    "Return the `cons' of virtualized `path-env' source and compiled file name."
-    (let ((_v_ (make-vdir "config/")))
-      `(cons ,(concat _v_ ".path-env.el")
-             ,(concat _v_ ".path-env.elc"))))
-
-  
-  (defun save-path-env ()
-    (let ((env (path-env)))
-      (set-path-env!)
-      (save-sexpr-to-file
-       (list 'progn
-             (list 'setenv "PATH" (getenv "PATH"))
-             (list 'setq 'exec-path (list 'quote exec-path)))
-       (car env))
-      (byte-compile-file (car env))))
+(defmacro load-path-env ()
+  `(let ((env (path-env)))
+     (if (file-exists-p (cdr env))
+         (load (cdr env))
+       (set-path-env!))
+     (add-hook 'kill-emacs-hook #'save-path-env)))
 
 
-  (defmacro load-path-env ()
-    `(let ((env (path-env)))
-       (if (file-exists-p (cdr env))
-           (load (cdr env))
-         (set-path-env!))
-       (add-hook 'kill-emacs-hook #'save-path-env))))
-
-
-;; set PATH on darwin
+;; set shell on darwin
 (platform-supported-when
     darwin
   (load-path-env))
 
 
-;; set PATH on Linux
+;; set shell on Linux
 (platform-supported-when
     gnu/linux
   (set-default-shell! "/bin/bash" "\/bash$")
   (load-path-env))
 
 
-;; set shell/ansi-term on Windows
+;; set shell on Windows
 (platform-supported-when
     windows-nt
 
+  
   (defmacro windows-nt-path (p)
     "Return the path that windows-nt can recoganized."
     `(replace-regexp-in-string "\\\\" "/" ,p))
 
-  
-  (defun set-windows-nt-shell ()
-    (when (bin-exists-p "bash")
-      (let ((prompt "~/.emacs_bash"))
-        (unless (file-exists-p prompt)
-          (copy-file "~/.emacs.d/config/.emacs_bash" prompt)))
-      (add-to-list 'exec-path
-                   (file-name-directory
-                    (windows-nt-path (bin-path "bash"))))
-      (setq shell-file-name "bash")
-      (setenv "SHELL" (bin-path "bash"))))
-  
-  (defadvice shell (before shell-before compile)
-    (set-windows-nt-shell))
-  
-  (defadvice ansi-term (around ansi-term-around compile)
-    (let* ((n "*ansi-term*")
-           (b (get-buffer-create n)))
-      (apply 'make-comint-in-buffer n b "cmd" nil nil)
-      (set-window-buffer (selected-window) b))))
+
+  (when (bin-exists-p "bash")
+    
+    (defun set-windows-nt-shell! ()
+      (setenv "SHELL" (bin-path "bash"))
+      (set-default-shell! (windows-nt-path (bin-path "bash")) "/bash\.exe$")
+      (load-path-env))
+
+
+    (defadvice shell (before shell-before compile)
+      (set-windows-nt-shell!))
+
+    
+    (defadvice ansi-term (around ansi-term-around compile)
+      (let* ((n "*ansi-term*")
+             (b (get-buffer-create n)))
+        (apply 'make-comint-in-buffer n b "cmd" nil nil)
+        (set-window-buffer (selected-window) b)))))
 
 
