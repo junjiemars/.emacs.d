216208e0264bca94dce837301ac60cb9582af97b
南山竹 <junjiemars@gmail.com>
2017-06-07 23:42:54 +0800

    self-ido-ubiquitous! fn into init hook #28

--- config/setup-navigation.el
+++ config/setup-navigation.el
@@ -7,25 +7,29 @@
 ;; switch buffers, and choose options from the minibuffer.
 ;; But it's loading slow, so load it after-init
 
-(add-hook
- 'after-init-hook
- (lambda ()
-   ;; This enables ido in all contexts where it could be useful, not just
-   ;; for selecting buffer and file names
-   (ido-ubiquitous-mode 1)
 
-   ;; Enhances M-x to allow easier execution of commands. Provides
-   ;; a filterable list of possible commands in the minibuffer
-   ;; http://www.emacswiki.org/emacs/Smex
-   ;; https://github.com/nonsequitur/smex
-   (global-set-key (kbd "M-x")
-                   (lambda ()
-                     (interactive)
-                     (or (boundp 'smex-cache)
-                         (smex-initialize))
-                     (smex)))
-   
-   (safe-setq smex-save-file (vdir! ".smex/" ".smex-items"))))
+(defun self-ido-ubiquitous! ()
+  ;; This enables ido in all contexts where it could be useful, not just
+  ;; for selecting buffer and file names
+  
+  (ido-ubiquitous-mode 1)
+
+  ;; Enhances M-x to allow easier execution of commands. Provides
+  ;; a filterable list of possible commands in the minibuffer
+  ;; http://www.emacswiki.org/emacs/Smex
+  ;; https://github.com/nonsequitur/smex
+  
+  (global-set-key (kbd "M-x")
+                  (lambda ()
+                    (interactive)
+                    (or (boundp 'smex-cache)
+                        (smex-initialize))
+                    (smex)))
+  
+  (safe-setq smex-save-file (vdir! ".smex/" ".smex-items")))
+
+
+(add-hook 'after-init-hook #'self-ido-ubiquitous!)
 
 
 
