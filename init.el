11b61055823054759edd9f83b04bebe59bfde1d5
南山竹 <junjiemars@gmail.com>
2017-06-08 15:05:04 +0800

    remove `custom-set-variables' block, #31

--- init.el
+++ init.el
@@ -372,17 +372,3 @@ self things.
 ;; ^ End of init.el
 
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-    (auctex cdlatex cider clojure-mode clojure-mode-extra-font-locking inf-clojure dockerfile-mode docker-tramp erlang lfe-mode ereader magit geiser slime sx))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
