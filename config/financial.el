01363948f4b5e263700ee3ea6cc0082813fe1686
南山竹 <junjiemars@gmail.com>
2017-06-10 18:59:09 +0800

    format docstring

--- config/financial.el
+++ config/financial.el
@@ -1,74 +1,105 @@
+;;;; -*- lexical-binding:t -*-
+;;;;
 ;; Financial Calculations
+;;;;
 
-(defmacro /. (DIVIDEND &rest DIVISORS)
-  "Returns first float-point DIVIDEND divided by all the remaining DIVISORS"
+
+
+
+(defmacro /. (dividend &rest divisors)
+  "Return first float-point DIVIDEND divided by all the remaining DIVISORS.
+
+\(fn DIVIDEND DIVISORS...\)"
   (declare (indent 1))
-  `(/ (+ ,DIVIDEND 0.0) ,@DIVISORS))
+  `(/ (+ ,dividend 0.0) ,@divisors))
+
 
 (defmacro rate. (R &optional periods)
-  "Returns the rate of R on the spedified periods."
+  "Return the rate of R on the spedified periods.
+
+\(fn RATE &optional PERIODS\)"
   (let ((rt (make-symbol "rt")))
     `(let ((,rt (if ,periods ,periods 1)))
        (/. ,R ,rt))))
 
+
 (defun interest (P R T &optional periods)
-  "Return the earned simple interest, P is the principal, R is the 
-  interest rate, and T is the times"
+  "Return the earned simple interest.
+
+\(fn PRINCIPAL RATE TIMES &optional PERIODS\)"
   (let ((R1 (rate. R periods)))
     (* P R1 T)))
 
+
 (defun future-value (P R T &optional periods)
-  "Returns the future value using simple interest. P is the principal, R is the
-  interest rate, and T is the times"
+  "Return the future value using simple interest.
+
+\(fn PRINCIPAL RATE TIMES &optional PERIODS\)"
   (let ((R1 (rate. R periods)))
     (* P (+ 1 (* R1 T)))))
 
+
 (defun future-value+ (P R T &optional periods)
-  "Returns the future value using compound interest. P is the principal, R is the
-  interest rate, and T is the times"
+  "Return the future value using compound interest.
+
+\(fn PRINCIPAL RATE TIMES &optional PERIODS\)"
   (let ((R1 (rate. R periods)))
     (* P (expt (+ 1 R1) T))))
 
+
 (defun interest+ (P R T &optional periods)
-  "Return the earned compound interest, P is the pricipal, R is the
-  interest rate, and T is times"
+  "Return the earned compound interest.
+
+\(fn PRINCIPAL RATE TIMES &optional PERIODS\)"
   (let ((R1 (rate. R periods)))
     (- (future-value+ P R1 T) P)))
 
+
 (defun present-value (F R T &optional periods)
-  "Returns the present value using simple interest. F is the future value, R is 
-  the interest rate, and T is the times"
+  "Return the present value using simple interest. 
+
+\(fn FUTURE-VALUE RATE TIMES &optional PERIODS\)"
   (let ((R1 (rate. R periods)))
     (/. F (+ 1 (* R1 T)))))
 
+
 (defun present-value+ (F R T &optional periods)
-  "Returns the present value using compound interest. F is the future value, R is
-  the interest rate, and T is the times"
+  "Return the present value using compound interest.
+
+\(fn FUTURE-VALUE RATE TIMES &optional PERIODS\)"
   (let ((R1 (rate. R periods)))
     (/. F (expt (+ 1 R1) T))))
 
+
 (defun times (F P R &optional periods)
-  "Returns the times using simple interest. F is the future value, P is the 
-  principal, and R is the interest rate"
+  "Return the times using simple interest.
+
+\(fn FUTURE-VALUE PRINCIPAL RATE &optional PERIODS\)"
   (let ((R1 (rate. R periods)))
     (/. (- (/. F P) 1) R1)))
 
+
 (defun times+ (F P R &optional periods)
-  "Returns the times using compound interest. F is the future value, P is the
-  principal, and R is the interest rate"
+  "Return the times using compound interest.
+
+\(fn FUTURE-VALUE PRINCIPAL RATE &optional PERIODS\)"
   (let ((R1 (rate. R periods)))
     (/. (log (/. F P)) (log (+ 1 R1)))))
 
+
 (defun rate (F P T &optional periods)
-  "Returns the simple interest rate. F is the future value, P is the principal,
-  and T is times"
+  "Return the simple interest rate.
+
+\(fn FUTURE-VALUE PRINCIPAL TIMES &optional PERIODS\)"
   (let ((R (/. (- (/. F P) 1) T))
         (rt (if periods periods 1)))
     (* R rt)))
 
+
 (defun rate+ (F P T &optional periods)
-  "Returns the compound interest rate. F is the future value, P is the principal,
-  and T is times"
+  "Return the compound interest rate.
+
+\(fn FUTURE-VALUE PRINCIPAL TIMES &optional PERIODS\)"
   (let ((R  (- (expt (/. F P) (/. 1 T)) 1))
         (rt (if periods periods 1)))
     (* R rt)))
