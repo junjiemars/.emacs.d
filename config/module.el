3d813f3b4e0fa36c98229471a27395bac107a289
南山竹 <junjiemars@gmail.com>
2017-06-30 23:34:38 +0800

    refine self-package-spec: if :packages is not empty then compile and load :compile section #44

--- config/module.el
+++ config/module.el
@@ -68,8 +68,9 @@
           (m (plist-get s :packages)))
       (when (or (and (booleanp p) p)
                 (funcall p))
-        (when (consp m) (install-package! m))
-        (apply #'compile-and-load-elisp-files! dir (plist-get s :compile))))))
+        (when (consp m)
+          (install-package! m)
+          (apply #'compile-and-load-elisp-files! dir (plist-get s :compile)))))))
 
 
 (defvar basic-package-spec
