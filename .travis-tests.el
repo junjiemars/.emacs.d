1bceb89c45a787f0811b2d1c511e593286a3427a
南山竹 <junjiemars@gmail.com>
2017-09-23 00:08:36 +0800

    travis test

--- .travis-tests.el
+++ .travis-tests.el
@@ -8,4 +8,16 @@
 (require 'ert)
 
 (ert-deftest %init:comment ()
-  (should (eq nil (comment (+ 1 2 3)))))
+  (should-not (comment))
+  (should-not (comment (+ 1 2 3)))
+  (should-not (comment (progn (+ 1) (* 2 3)))))
+
+(ert-deftest %init:emacs-home ()
+  (if (boundp 'user-emacs-directory)
+      (should (string= emacs-home user-emacs-directory))
+    (should (string= emacs-home "~/.emacs.d/"))))
+
+(ert-deftest %init:emacs-home* ()
+  (should (string-match "\.emacs\.d/$" (emacs-home*)))
+  (should (string-match "\.emacs\.d/config/$" (emacs-home* "config/")))
+  (should (string-match "\.emacs\.d/x/y/z/$" (emacs-home* "x/" "y/" "z/")))) 
