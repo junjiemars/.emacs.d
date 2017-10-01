c7d0c9c7e45ba3c997c4d2cffdf7fd7ebd03a1b7
南山竹 <junjiemars@gmail.com>
2017-10-01 13:30:38 +0800

    refine `refine-path-var' performance

--- config/shell.el
+++ config/shell.el
@@ -87,14 +87,13 @@ get via (path-env-> k) and put via (path-env<- k v) ")
 (defmacro refine-path-var (var)
   "Refine PATH which be specified by VAR."
   `(refine-path
-    (split-string (refine-var
-                   (echo-var (path-env-spec-> ,var)) "[ ]*\n$" "")
+    (split-string (refine-var (echo-var ,var) "[ ]*\n$" "")
                   path-separator)))
 
 
 (defun save-path-env! ()
-  (path-env<- :path (refine-path-var :path-var))
-  (path-env<- :ld-path (refine-path-var :ld-path-var))
+  (path-env<- :path (refine-path-var (path-env-spec->% :path-var)))
+  (path-env<- :ld-path (refine-path-var (path-env-spec->% :ld-path-var)))
   (path-env<- :shell-file-name nil)
   (save-sexpr-to-file
    (list 'setq '*default-path-env*
@@ -111,7 +110,7 @@ get via (path-env-> k) and put via (path-env<- k v) ")
   `(progn
      (if (file-exists-p (path-env-spec->% :compiled-file))
          (load (path-env-spec->% :compiled-file))
-       (path-env<- :path (refine-path-var :path-var)))
+       (path-env<- :path (refine-path-var (path-env-spec->% :path-var))))
      (add-hook 'kill-emacs-hook #'save-path-env!)))
 
 
