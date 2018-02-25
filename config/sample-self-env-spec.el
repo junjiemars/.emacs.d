2e199fb8b9f31955e2ddecf3b2b4a27a1b8eb098
南山竹 <junjiemars@gmail.com>
2018-02-25 16:35:24 +0800

    default :desktop in env-spec

--- config/sample-self-env-spec.el
+++ config/sample-self-env-spec.el
@@ -6,21 +6,21 @@
 
 
 
-;; Basic Emacs' GUI environment configurations:
-;; you can customize your `:theme', `:font', `:cjk-font',
-;; `:shell', `:desktop' or `:socks',
-;; all those can be Gited in yourself Git repository.
+;; Basic Emacs' environment configurations
 
 
 (def-self-env-spec
   :theme (list :name 'atom-one-dark
                :path (emacs-home* "theme/")
                :allowed t)
+  
   :font (list :name "Monaco-13"
               :allowed nil)
+  
   :cjk-font (list :name "Microsoft Yahei"
                   :size 13
                   :allowed nil)
+  
   :shell (list :env-vars `("JAVA_HOME"
                            "PYTHONPATH"
                            ,(platform-supported-unless windows-nt
@@ -31,18 +31,21 @@
                :exec-path t
                :bin-path (comment `,(bin-path "bash"))
                :allowed nil)
+  
   :desktop (list :files-not-to-save
                  ".*\.t?gz\\|\.desktop\\|~$\\|\\/ssh[: ]"
                  :buffers-not-to-save "^TAGS\\|\\.log"
                  :modes-not-to-save
                  '(dired-mode fundamental-mode rmail-mode)
                  :restore-eager 8
-                 :allowed t)
+                 :allowed nil)
+  
   :eshell (list :visual-commands '("mtr")
                 :destroy-buffer-when-process-dies t
                 :visual-subcommands '(("git" "log"))
                 :visual-options nil
                 :allowed nil)
+  
   :socks (list :port 32000
                :server "127.0.0.1"
                :version 5
