7b1ebb9261e5b9fd462b0388044c11c159ec4437
南山竹 <junjiemars@gmail.com>
2018-02-27 19:58:32 +0800

    versioned setq tramp-persistency-file-name

--- config/basic.el
+++ config/basic.el
@@ -12,7 +12,9 @@
 (setq-default eshell-directory-name (v-home! ".eshell/"))
 (setq-default server-auth-dir (v-home! ".server/"))
 (setq-default image-dired-dir (v-home! ".image-dired/"))
-(setq-default tramp-persistency-file-name (v-home! ".tramp/" "tramp"))
+(version-supported-when <= 23
+  (setq-default tramp-persistency-file-name (v-home! ".tramp/" "tramp")))
+
 
 ;; versioned dirs: load-path
 (eval-when-compile
