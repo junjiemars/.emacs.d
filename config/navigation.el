60c67ad6d9f03d24e5618eb381fd4e7397c4582e
南山竹 <junjiemars@gmail.com>
2018-02-28 23:33:42 +0800

    remove toggle-recentf-mode macro
    add set-default-modes! autoload fn

--- config/navigation.el
+++ config/navigation.el
@@ -1,3 +1,4 @@
+;;;; -*- lexical-binding:t -*-
 ;;;;
 ;; Navigation
 ;;;;
@@ -6,30 +7,6 @@
 ;; These customizations make it easier for you to navigate files,
 ;; switch buffers, and choose options from the minibuffer.
 ;; But be care for some operations will slowdown Emacs
-
-
-
-;; Toggle recentf-mode
-(defmacro toggle-recentf-mode (&optional disable)
-  "Toggle recentf-mode, disable recentf-mode unconditional when `disable' is non-nil."
-  `(cond (,disable (recentf-mode -1))
-         ((or (not (boundp 'recentf-mode))
-              (null recentf-mode))
-          ;; (setq-default recentf-auto-cleanup 'never)
-          ;; recentf-list
-          (setq-default recentf-max-saved-items 8)
-          (recentf-mode 1))
-         (t (recentf-mode -1))))
-
-
-
-;; ido-mode allows you to more easily navigate choices. For example,
-;; when you want to switch buffers, ido presents you with a list
-;; of buffers in the the mini-buffer. As you start to type a buffer's
-;; name, ido will narrow down the list of buffers to match the text
-;; you've typed in
-;; http://www.emacswiki.org/emacs/InteractivelyDoThings
-(ido-mode t)
 
 
 
