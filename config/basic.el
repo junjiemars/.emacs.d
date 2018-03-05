118a786ded791534e215ca0364f6660f7f6a2b07
南山竹 <junjiemars@gmail.com>
2018-03-06 00:35:27 +0800

    require saveplace on compile-time

--- config/basic.el
+++ config/basic.el
@@ -61,7 +61,8 @@
 ;; When you visit a file, point goes to the last place where it
 ;; was when you previously visited the same file.
 ;; http://www.emacswiki.org/emacs/SavePlace
-(setq-default save-place-file (v-home! ".places/" "places"))
+(eval-when-compile (require 'saveplace))
+(setq save-place-file (v-home! ".places/" "places"))
 
 ;; Server
 (setq-default server-auth-dir (v-home! ".server/"))
