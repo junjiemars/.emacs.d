afdffab8e3af360ff35092fb14fc8502f6f6ed15
南山竹 <junjiemars@gmail.com>
2018-03-06 15:20:52 +0800

    replace safe-setq with setq% in eshells.el

--- config/eshells.el
+++ config/eshells.el
@@ -15,12 +15,12 @@
    (when (self-spec->* :eshell :allowed)
      (dolist (x (self-spec->* :eshell :visual-commands))
        (add-to-list 'eshell-visual-commands x t #'string=))
-     (safe-setq eshell-destroy-buffer-when-process-dies
-                (self-spec->* :eshell :destroy-buffer-when-process-dies))
-     (safe-setq eshell-visual-subcommands
-                (self-spec->* :eshell :visual-subcommands))
-     (safe-setq eshell-visual-options
-                (self-spec->* :eshell :visual-options)))))
+     (setq% eshell-destroy-buffer-when-process-dies
+	    (self-spec->* :eshell :destroy-buffer-when-process-dies))
+     (setq% eshell-visual-subcommands
+	    (self-spec->* :eshell :visual-subcommands))
+     (setq% eshell-visual-options
+	    (self-spec->* :eshell :visual-options)))))
 
 
 (defadvice eshell (after eshell-after compile)
