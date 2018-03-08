1bee4b4598581470a14505e5946224b74f893891
南山竹 <junjiemars@gmail.com>
2018-03-08 17:10:48 +0800

    refine memory.el with self-spec->*env-spec macro

--- config/memory.el
+++ config/memory.el
@@ -22,20 +22,18 @@ at compile time."
 
 
 (defun save-env-spec! ()
-  (self-safe-call
-   "env-spec"
-   (let ((f (env-spec->% :source)))
-     (when (save-sexp-to-file
-            `(setq *self-previous-env-spec* ',*val*) f)
-       (byte-compile-file f)))))
+  (when (save-sexp-to-file
+	 `(setq *self-previous-env-spec*
+		',(self-spec->*env-spec))
+	 (env-spec->% :source))
+    (byte-compile-file (env-spec->% :source))))
 
 (add-hook 'kill-emacs-hook #'save-env-spec! t)
 
 
 (defmacro load-env-spec ()
-  `(let ((f (env-spec->% :compiled)))
-     (when (file-exists-p f)
-       (load f))))
+  `(when (file-exists-p (env-spec->% :compiled))
+     (load (env-spec->% :compiled))))
 
 (load-env-spec)
 
@@ -76,18 +74,16 @@ at compile time."
       (version-supported-when > 25
         (setq% desktop-restore-forces-onscreen nil desktop))))
 
-  (self-safe-call
-   "env-spec"
-   (when (and (self-spec->* :desktop :allowed)
-              (file-exists-p (v-home* ".desktop/")))
-     (theme-supported-p
-         (when (consp (theme-changed-p
-                       (self-spec-> *self-previous-env-spec* :theme)
-                       (self-spec->* :theme)))
-           (setq% desktop-restore-frames nil desktop)))
-     (setq% desktop-restore-eager
-	    (self-spec->* :desktop :restore-eager) desktop)
-     (desktop-read (v-home* ".desktop/")))))
+  (when (and (self-spec->*env-spec :desktop :allowed)
+	     (file-exists-p (v-home* ".desktop/")))
+    (theme-supported-p
+	(when (consp (theme-changed-p
+		      (self-spec-> *self-previous-env-spec* :theme)
+		      (self-spec->*env-spec :theme)))
+	  (setq% desktop-restore-frames nil desktop)))
+    (setq% desktop-restore-eager
+	   (self-spec->*env-spec :desktop :restore-eager) desktop)
+    (desktop-read (v-home* ".desktop/"))))
 
 
 (add-hook 'after-init-hook #'self-desktop-read! t)
@@ -99,28 +95,25 @@ at compile time."
 
 ;; Save desktop
 (defun self-desktop-save! ()
-  (self-safe-call
-   "env-spec"
-   
-   (when (self-spec->* :desktop :allowed)
-     (let ((f (self-spec->* :desktop :files-not-to-save)))
-       (when f
-         (setq% desktop-files-not-to-save f desktop)))
-     (let ((b (self-spec->* :desktop :buffers-not-to-save)))
-       (when b
-         (setq% desktop-buffers-not-to-save b desktop)))
-     (let ((m (self-spec->* :desktop :modes-not-to-save)))
-       (setq% desktop-modes-not-to-save
-	      (append '(tags-table-mode) m) desktop))
+  (when (self-spec->*env-spec :desktop :allowed)
+    (let ((f (self-spec->*env-spec :desktop :files-not-to-save)))
+      (when f (setq% desktop-files-not-to-save f desktop)))
+    
+    (let ((b (self-spec->*env-spec :desktop :buffers-not-to-save)))
+      (when b (setq% desktop-buffers-not-to-save b desktop)))
+    
+    (let ((m (self-spec->*env-spec :desktop :modes-not-to-save)))
+      (setq% desktop-modes-not-to-save
+	     (append '(tags-table-mode) m) desktop))
 
-     (theme-supported-p
-         (switch-theme! (self-spec-> *self-previous-env-spec* :theme)
-                        (self-spec->* :theme)))
+    (theme-supported-p
+	(switch-theme! (self-spec-> *self-previous-env-spec* :theme)
+		       (self-spec->*env-spec :theme)))
 
-     (version-supported-if
-         >= 23
-         (desktop-save (v-home! ".desktop/"))
-       (desktop-save (v-home! ".desktop/") t)))))
+    (version-supported-if
+	>= 23
+	(desktop-save (v-home! ".desktop/"))
+      (desktop-save (v-home! ".desktop/") t))))
 
 
 (add-hook 'kill-emacs-hook #'self-desktop-save! t)
