3ee22eb3c428b3d84f63e7c3175ab6152a2b85ca
南山竹 <junjiemars@gmail.com>
2018-04-20 14:08:16 +0800

    rollback with-eval-after-load macro
    in Emacs-23.3.1:
    1) (funcall (lambda () ...)) works rightly.
    2) (with-eval-after-load 'semantic ...) will trigger the error:
    attempt to modify readonly error. I'd not found solution fot that.

--- config/sample-self-epilogue.el
+++ config/sample-self-epilogue.el
@@ -40,32 +40,31 @@
  ;; correctly and more faster
  ;; `system-cc-include' support any platform
  (feature-semantic-supported-p
-	 (with-eval-after-load 'semantic
-		 (add-hook 'semantic-mode-hook
-							 #'(lambda ()
-									 (platform-supported-if windows-nt
-											 (use-cc `("e:/apps/c/out/"
-																 "e:/apps/c/src/hi/"
-																 "e:/apps/c/src/lang/"
-																 "e:/apps/c/src/io/"
-																 "e:/apps/c/src/posix/"
-																 "d:/opt/open/ecl/"
-																 "d:/opt/open/ecl/build/"
-																 "d:/opt/open/gambit/"
-																 ,source-directory)
-															 `("e:/apps/c/"
-																 ,source-directory))
-										 (use-cc `("/opt/apps/c/out/"
-															 "/opt/apps/c/src/hi/"
-															 "/opt/apps/c/src/lang/"
-															 "/opt/apps/c/src/io/"
-															 "/opt/apps/c/src/posix/"
-															 "/opt/opt/open/ecl/"
-															 "/opt/open/ecl/build/"
-															 "/opt/open/gambit/"
+	 (add-hook 'semantic-mode-hook
+						 #'(lambda ()
+								 (platform-supported-if windows-nt
+										 (use-cc `("e:/apps/c/out/"
+															 "e:/apps/c/src/hi/"
+															 "e:/apps/c/src/lang/"
+															 "e:/apps/c/src/io/"
+															 "e:/apps/c/src/posix/"
+															 "d:/opt/open/ecl/"
+															 "d:/opt/open/ecl/build/"
+															 "d:/opt/open/gambit/"
 															 ,source-directory)
-														 `("/opt/apps/c/"
-															 ,source-directory)))) t))))
+														 `("e:/apps/c/"
+															 ,source-directory))
+									 (use-cc `("/opt/apps/c/out/"
+														 "/opt/apps/c/src/hi/"
+														 "/opt/apps/c/src/lang/"
+														 "/opt/apps/c/src/io/"
+														 "/opt/apps/c/src/posix/"
+														 "/opt/opt/open/ecl/"
+														 "/opt/open/ecl/build/"
+														 "/opt/open/gambit/"
+														 ,source-directory)
+													 `("/opt/apps/c/"
+														 ,source-directory)))) t)))
 
 
 (comment
