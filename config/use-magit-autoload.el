adc942ae977cfec6e110952ead39bf2a313b3283
南山竹 <junjiemars@gmail.com>
2018-04-20 16:32:40 +0800

    improved: use-magit! delay loading
    default key binding: "C-c g s" => magit-status

--- config/use-magit-autoload.el
+++ config/use-magit-autoload.el
@@ -4,22 +4,26 @@
 ;;
 
 
-(when-fn%  magit-status magit
-  (global-set-key (kbd "C-c g s") 'magit-status))
-(when-fn% magit-pull magit
-  (global-set-key (kbd "C-c g p") 'magit-pull))
-(when-fn% magit-push magit
-  (global-set-key (kbd "C-c g P") 'magit-push))
-(when-fn% magit-log magit
-  (global-set-key (kbd "C-c g l") 'magit-log))
-(when-fn% magit-log-buffer-file magit
-  (global-set-key (kbd "C-c g b") 'magit-log-buffer-file))
-(when-fn% magit-checkout magit
-  (global-set-key (kbd "C-c g c") 'magit-checkout))
-(when-fn% magit-merge magit
-  (global-set-key (kbd "C-c g m") 'magit-merge))
-(when-fn% magit-fetch magit
-  (global-set-key (kbd "C-c g f") 'magit-fetch))
+(defun use-magit! ()
+	(when-fn% magit-pull magit
+		(global-set-key (kbd "C-c g p") 'magit-pull))
+	(when-fn% magit-push magit
+		(global-set-key (kbd "C-c g P") 'magit-push))
+	(when-fn% magit-log magit
+		(global-set-key (kbd "C-c g l") 'magit-log))
+	(when-fn% magit-log-buffer-file magit
+		(global-set-key (kbd "C-c g b") 'magit-log-buffer-file))
+	(when-fn% magit-checkout magit
+		(global-set-key (kbd "C-c g c") 'magit-checkout))
+	(when-fn% magit-merge magit
+		(global-set-key (kbd "C-c g m") 'magit-merge))
+	(when-fn% magit-fetch magit
+		(global-set-key (kbd "C-c g f") 'magit-fetch)))
 
 
+(with-eval-after-load 'magit
+	(use-magit!))
 
+
+(when-fn% magit-status magit
+  (global-set-key (kbd "C-c g s") #'magit-status))
