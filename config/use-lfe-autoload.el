a0450fa7191f810882500a70d52ff28c3d7e3445
南山竹 <junjiemars@gmail.com>
2018-04-20 21:33:06 +0800

    with-eval-after-load use-lfe-autoload.el

--- config/use-lfe-autoload.el
+++ config/use-lfe-autoload.el
@@ -4,11 +4,13 @@
 ;;;;
 
 
-;; Enable paredit for LFE
-(add-hook 'lfe-mode-hook #'enable-paredit-mode)
+(with-eval-after-load 'lfe-mode
+	
+	;; Enable paredit for LFE
+	(add-hook 'lfe-mode-hook #'enable-paredit-mode)
 
-;; For .lfe files
-(add-to-list 'auto-mode-alist '("\\.lfe\\'" . lfe-mode))
+	;; For .lfe files
+	(add-to-list 'auto-mode-alist '("\\.lfe\\'" . lfe-mode)))
 
 
 
