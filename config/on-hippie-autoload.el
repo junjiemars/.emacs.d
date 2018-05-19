09135944c2d40bdbd117568385a90bd1c66c537d
南山竹 <junjiemars@gmail.com>
2018-05-19 11:43:59 +0800

    try-complete-file-name

--- config/on-hippie-autoload.el
+++ config/on-hippie-autoload.el
@@ -7,13 +7,18 @@
 ;;;;
 
 
-;; Lisp-friendly hippie expand
-(setq hippie-expand-try-functions-list
-			'(try-expand-dabbrev
-				try-expand-dabbrev-all-buffers
-				try-expand-dabbrev-from-kill
-				try-complete-lisp-symbol-partially
-				try-complete-lisp-symbol))
+(setq% hippie-expand-try-functions-list
+			 '(try-complete-file-name-partially
+				 try-complete-file-name
+				 try-expand-all-abbrevs
+				 ;; try-expand-list
+				 ;; try-expand-line
+				 try-expand-dabbrev
+				 try-expand-dabbrev-all-buffers
+				 try-expand-dabbrev-from-kill
+				 try-complete-lisp-symbol-partially
+				 try-complete-lisp-symbol)
+			 hippie-exp)
 
 
 ;; Key binding to use "hippie expand" for text autocompletion
