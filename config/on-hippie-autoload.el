7215154e858107407f09b20cec782b9edc149fd4
南山竹 <junjiemars@gmail.com>
2018-05-20 10:20:42 +0800

    with-eval-after-load 'hippie-exp

--- config/on-hippie-autoload.el
+++ config/on-hippie-autoload.el
@@ -7,23 +7,24 @@
 ;;;;
 
 
-(setq% hippie-expand-try-functions-list
-			 '(try-complete-file-name-partially
-				 try-complete-file-name
-				 try-expand-all-abbrevs
-				 ;; try-expand-list
-				 ;; try-expand-line
-				 try-expand-dabbrev
-				 try-expand-dabbrev-all-buffers
-				 try-expand-dabbrev-from-kill
-				 try-complete-lisp-symbol-partially
-				 try-complete-lisp-symbol)
-			 hippie-exp)
+(with-eval-after-load 'hippie-exp
+	(setq% hippie-expand-try-functions-list
+				 '(try-complete-file-name-partially
+					 try-complete-file-name
+					 try-expand-all-abbrevs
+					 ;; try-expand-list
+					 ;; try-expand-line
+					 try-expand-dabbrev
+					 try-expand-dabbrev-all-buffers
+					 try-expand-dabbrev-from-kill
+					 try-complete-lisp-symbol-partially
+					 try-complete-lisp-symbol)
+				 hippie-exp))
 
 
 ;; Key binding to use "hippie expand" for text autocompletion
 ;; http://www.emacswiki.org/emacs/HippieExpand
-(global-set-key (kbd "M-/") 'hippie-expand)
+(global-set-key (kbd "M-/") #'hippie-expand)
 
 
 
