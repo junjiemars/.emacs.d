89410fd8f7d34fc4d3489d120576d1db5899ee59
南山竹 <junjiemars@gmail.com>
2018-05-22 11:20:40 +0800

    refine docstring

--- config/strap.el
+++ config/strap.el
@@ -74,7 +74,7 @@ If (eq `system-type' OS) yields nil, and there are no ELSE’s, the value is nil
 
 
 (defmacro setq% (x val &optional feature)
-  "Set X when variable X is bound.
+  "Set X to the value of its VAL when variable X is bound.
 If X requires the FEATURE load it on compile-time."
   ;; (declare (debug t))
   (when feature (require feature nil t))
