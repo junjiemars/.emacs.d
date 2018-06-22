4978cd51570f37b843846c1815bd2be2aaa4ab26
南山竹 <junjiemars@gmail.com>
2018-06-22 11:29:43 +0800

    gud-lldb-massage-args

--- config/gud-lldb.el
+++ config/gud-lldb.el
@@ -13,8 +13,14 @@
 ;; lldb functions
 
 
-(defvar gud-lldb-history nil
-	"History of argument lists passed to lldb.")
+(defcustom gud-lldb-history nil
+	"History of argument lists passed to lldb."
+	:group 'gud)
+
+(defcustom gud-lldb-options-hook nil
+	"Hook run by `lldb'."
+	:type 'hook
+	:group 'gud)
 
 ;; Keeps track of breakpoint created.  In the following case, the id is "1".
 ;; It is used to implement temporary breakpoint.
@@ -22,6 +28,10 @@
 ;; breakpoint set --file 'main.c' --line 39
 ;; Breakpoint created: 1: file ='main.c', line = 39, locations = 1
 (defvar gud-breakpoint-id nil)
+
+(defvar lldb-oneshot-break-defined nil
+	"Keeps track of whether the Python lldb_oneshot_break function 
+definition has been executed.")
 
 (defun lldb-extract-breakpoint-id (string)
   ;; Search for "Breakpoint created: \\([^:\n]*\\):" pattern.
@@ -62,9 +72,36 @@
     (setq gud-marker-acc (substring gud-marker-acc (or start 0))))
 	string)
 
-;; Keeps track of whether the Python lldb_oneshot_break function definition
-;; has been exec'ed.
-(defvar lldb-oneshot-break-defined nil)
+(defun gud-lldb-tbreak ()
+	(progn
+		(gud-call "breakpoint set -f %f -l %l")
+		(sit-for 1)
+		(if (not lldb-oneshot-break-defined)
+				(progn
+					;; The "\\n"'s are required to escape the newline chars
+					;; passed to the lldb process.
+					(gud-call
+					 (concat "script exec \"def lldb_oneshot_break(frame, bp_loc):\\n"
+									 "    target=frame.GetThread().GetProcess().GetTarget()\\n"
+									 "    bp=bp_loc.GetBreakpoint()\\n"
+									 "    print 'Deleting oneshot breakpoint:', bp\\n"
+									 "    target.BreakpointDelete(bp.GetID())\""))
+					(sit-for 1)
+					;; Set the flag since Python knows about the function def now.
+					(setq lldb-oneshot-break-defined t)))
+		(gud-call "breakpoint command add -p %b -o 'lldb_oneshot_break(frame, bp_loc)'")))
+
+(defun gud-lldb-massage-args (file args)
+	"As the 2nd argument:message-args of `gud-common-init'.
+
+lldb [options]:
+  -c \"<command>\" executes the given debugger command at the first debugger
+  -lines requests that line number information be used if present"
+	(let ((options nil))
+		(dolist (o gud-lldb-options-hook options)
+			(append options (funcall o)))
+		(append options args)))
+
 
 (defun lldb (command-line)
   "Run lldb on program FILE in buffer *gud-FILE*.
@@ -72,95 +109,56 @@ The directory containing FILE becomes the initial working directory
 and source-file directory for your debugger."
   (interactive (list (gud-query-cmdline 'lldb)))
 
-  (gud-common-init command-line nil 'gud-lldb-marker-filter)
+  (gud-common-init command-line 'gud-lldb-massage-args 'gud-lldb-marker-filter)
   (set (make-local-variable 'gud-minor-mode) 'lldb)
   (setq lldb-oneshot-break-defined nil)
 
-  ;; Make lldb dump fullpath instead of basename for a file.
-  ;; See also gud-lldb-marker-filter where gud-last-frame is grokked from
-	;; lldb output.
-  (progn
-    (gud-call "settings set frame-format frame #${frame.index}: ${frame.pc}{ ${module.file.basename}{`${function.name}${function.pc-offset}}}{ at ${line.file.fullpath}:${line.number}}\\n")
-    (sit-for 1)
-    (gud-call "settings set thread-format thread #${thread.index}: tid = ${thread.id}{, ${frame.pc}}{ ${module.file.basename}{`${function.name}${function.pc-offset}}}{ at ${line.file.fullpath}:${line.number}}{, stop reason = ${thread.stop-reason}}\\n")
-    (sit-for 1))
-
   (gud-def gud-listb "breakpoint list"
 					 "l" "List all breakpoints.")
-	
   (gud-def gud-bt "thread backtrace"
 					 "b" "Show stack for the current thread.")
-	
   (gud-def gud-bt-all "thread backtrace all"
 					 "B" "Show stacks for all the threads.")
-
   (gud-def gud-break "breakpoint set -f %f -l %l"
 					 "\C-b" "Set breakpoint at current line.")
-  (gud-def gud-tbreak
-					 (progn
-						 (gud-call "breakpoint set -f %f -l %l")
-						 (sit-for 1)
-						 (if (not lldb-oneshot-break-defined)
-								 (progn
-									 ;; The "\\n"'s are required to escape the newline chars
-									 ;; passed to the lldb process.
-									 (gud-call
-										(concat "script exec \"def lldb_oneshot_break(frame, bp_loc):\\n"
-														"    target=frame.GetThread().GetProcess().GetTarget()\\n"
-														"    bp=bp_loc.GetBreakpoint()\\n"
-														"    print 'Deleting oneshot breakpoint:', bp\\n"
-														"    target.BreakpointDelete(bp.GetID())\""))
-									 (sit-for 1)
-									 ;; Set the flag since Python knows about the function def now.
-									 (setq lldb-oneshot-break-defined t)))
-						 (gud-call "breakpoint command add -p %b -o 'lldb_oneshot_break(frame, bp_loc)'"))
+  (gud-def gud-tbreak (gud-lldb-tbreak)
 					 "\C-t" "Set temporary breakpoint at current line.")
-	
   (gud-def gud-remove "breakpoint clear -f %f -l %l"
 					 "\C-d" "Remove breakpoint at current line")
-	
   (gud-def gud-step "thread step-in"
 					 "\C-s" "Step one source line with display.")
-	
-  (gud-def gud-stepi "thread step-inst"
+	(gud-def gud-stepi "thread step-inst"
 					 "\C-i" "Step one instruction with display.")
 	
   (gud-def gud-next "thread step-over"
 					 "\C-n" "Step one line (skip functions).")
-	
   (gud-def gud-nexti "thread step-inst-over"
 					 nil    "Step one instruction (skip functions).")
-	
   (gud-def gud-cont "process continue"
 					 "\C-r" "Continue with display.")
-	
   (gud-def gud-finish "thread step-out"
 					 "\C-f" "Finish executing current function.")
-	
   (gud-def gud-up
            (progn (gud-call "frame select -r 1")
                   (sit-for 1))
 					 "<" "Up 1 stack frame.")
-	
   (gud-def gud-down
            (progn (gud-call "frame select -r -1")
                   (sit-for 1))
 					 ">" "Down 1 stack frame.")
-	
   (gud-def gud-print "expression -- %e"
 					 "\C-p" "Evaluate C expression at point.")
-	
   (gud-def gud-pstar "expression -- *%e"
 					 nil "Evaluate C dereferenced pointer expression at point.")
-	
   (gud-def gud-run "run"
 					 "r" "Run the program.")
   (gud-def gud-stop-subjob "process kill"
 					 "s" "Stop the program.")
-
-  (setq comint-prompt-regexp  "^\\(lldb\\)[ \t]*")
+  (setq comint-prompt-regexp  "^(lldb)[ \t]*")
   (setq paragraph-start comint-prompt-regexp)
   (run-hooks 'lldb-mode-hook))
+
+
 
 
 (provide 'gud-lldb)
