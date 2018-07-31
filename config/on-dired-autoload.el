eeb947b8b702c4e6762cfe16ff7397d1b382a423
南山竹 <junjiemars@gmail.com>
2018-07-31 10:55:00 +0800

    clean on-dired-autoload.el at compile time

--- config/on-dired-autoload.el
+++ config/on-dired-autoload.el
@@ -7,51 +7,47 @@
 ;;;;
 
 
-
-(with-eval-after-load 'dired-aux
-
-	(unless-fn% dired-do-compress-to dired-aux
-		;; on ancent Emacs, `dired' can't recognize .zip archive.
-		;; [Z] key should be recognize .zip extension and uncompress a .zip archive.
-		;; [! zip x.zip ?] compress marked files to x.zip
-		(when (and (executable-find% "zip")
-							 (executable-find% "unzip"))
-			(when-var%
-			 dired-compress-file-suffixes dired-aux
-			 (unless (assoc** "\\.zip\\'" dired-compress-file-suffixes #'string=)
-				 (add-to-list 'dired-compress-file-suffixes
-											'("\\.zip\\'" ".zip" "unzip"))))))
-
-	(platform-supported-when
-      windows-nt
-    ;; on Windows: there are no builtin zip program
-    ;; so try to use minzip in Emacs dep for Windows.
-    ;; see `dired-do-compress-to'.
-    (when-var%
-		 dired-compress-files-alist dired-aux
-		 (unless (executable-find% "zip")
-			 (require 'dired-aux)
-			 (let ((zip (assoc** "\\.zip\\'" dired-compress-files-alist #'string=)))
-				 (when (and zip (string-match-p "^zip" (cdr zip))
-										(executable-find% "minizip"))
-					 (setcdr zip "minizip %o -9 %i")))))
-
-    (when (executable-find%
-					 "ls"
-					 (lambda (ls)
-						 (let ((ver (shell-command* ls "--version")))
-							 (when (zerop (car ver))
-								 (string-match "^ls (GNU coreutils)"
-															 (cdr ver))))))
-      ;; prefer GNU's ls on Windows
-      ;; on Windows: `dired-mode' does not display executable flag in file mode
-      (setq% ls-lisp-use-insert-directory-program t ls-lisp))))
+(if-fn% dired-do-compress-to dired-aux
+				(platform-supported-when windows-nt
+					;; on Windows: there are no builtin zip program
+					;; so try to use minzip in Emacs dep for Windows.
+					;; see `dired-do-compress-to'.
+					(when-var% dired-compress-files-alist dired-aux
+										 (with-eval-after-load 'dired-aux
+											 (unless (executable-find% "zip")
+												 ;; `format-spec' may not autoload
+												 (require 'format-spec)
+												 (let ((zip (assoc** "\\.zip\\'" dired-compress-files-alist #'string=)))
+													 (when (and zip (string-match-p "^zip" (cdr zip))
+																			(executable-find% "minizip"))
+														 (setcdr zip "minizip %o -9 %i")))))))
+	(when-var% dired-compress-file-suffixes dired-aux
+						 ;; on ancent Emacs, `dired' can't recognize .zip archive.
+						 ;; [Z] key should be recognize .zip extension and uncompress a .zip archive.
+						 ;; [! zip x.zip ?] compress marked files to x.zip
+						 ;; see `dired-compress-file-suffixes'.
+						 (with-eval-after-load 'dired-aux
+							 (when (and (executable-find% "zip")
+													(executable-find% "unzip"))
+								 (unless (assoc** "\\.zip\\'" dired-compress-file-suffixes #'string=)
+									 (add-to-list 'dired-compress-file-suffixes
+																'("\\.zip\\'" ".zip" "unzip")))))))
 
 
-(with-eval-after-load 'dired
+(platform-supported-unless gnu/linux
 
-  (platform-supported-when
-      darwin
-    ;; on Drawin: ls does not support --dired option
-    ;; see `dired-use-ls-dired' for more defails
-    (setq% dired-use-ls-dired nil dired)))
+	(with-eval-after-load 'ido
+		;; see `ido-dired'
+		(if (executable-find% "ls"
+													(lambda (ls)
+														(let ((ver (shell-command* ls "--version")))
+															(when (zerop (car ver))
+																(string-match "^ls (GNU coreutils)"
+																							(cdr ver))))))
+				;; prefer GNU's ls on Windows or Darwin
+				;; on Windows: `dired-mode' does not display executable flag in file mode
+				;; see `dired-use-ls-dired' for more defails
+				(setq% ls-lisp-use-insert-directory-program t ls-lisp)
+			(platform-supported-when darwin
+				;; on Drawin: ls does not support --dired option
+				(setq% dired-use-ls-dired nil dired)))))
