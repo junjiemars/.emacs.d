a7d83d97a9faceeadd99bfa55d8599af1f98d338
南山竹 <junjiemars@gmail.com>
2018-07-31 12:43:26 +0800

    move enable ido into on-dired-autoload.el

--- config/on-dired-autoload.el
+++ config/on-dired-autoload.el
@@ -51,3 +51,14 @@
 			(platform-supported-when darwin
 				;; on Drawin: ls does not support --dired option
 				(setq% dired-use-ls-dired nil dired)))))
+
+
+;; ido-mode allows you to more easily navigate choices. For example,
+;; when you want to switch buffers, ido presents you with a list
+;; of buffers in the the mini-buffer. As you start to type a buffer's
+;; name, ido will narrow down the list of buffers to match the text
+;; you've typed in
+;; http://www.emacswiki.org/emacs/InteractivelyDoThings
+(ido-mode t)
+
+
--- config/on-edit-autoload.el
+++ config/on-edit-autoload.el
@@ -30,13 +30,6 @@
 ;; Highlights matching parenthesis
 (show-paren-mode 1)
 
-;; ido-mode allows you to more easily navigate choices. For example,
-;; when you want to switch buffers, ido presents you with a list
-;; of buffers in the the mini-buffer. As you start to type a buffer's
-;; name, ido will narrow down the list of buffers to match the text
-;; you've typed in
-;; http://www.emacswiki.org/emacs/InteractivelyDoThings
-(ido-mode t)
 
 ;; enable save minibuffer history
 (version-supported-if
