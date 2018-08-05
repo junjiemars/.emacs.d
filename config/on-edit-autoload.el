52bafa59aff5c5ffab96703f5bea0f54fc2a195a
南山竹 <junjiemars@gmail.com>
2018-08-05 23:38:52 +0800

    format

--- config/on-edit-autoload.el
+++ config/on-edit-autoload.el
@@ -96,19 +96,19 @@
   (interactive)
   (let (begin end)
     (if-fn% region-active-p nil
-	    (if (region-active-p)
-		(setq begin (region-beginning)
-		      end (region-end))
-	      (setq begin (line-beginning-position)
-		    end (line-end-position)))
+						(if (region-active-p)
+								(setq begin (region-beginning)
+											end (region-end))
+							(setq begin (line-beginning-position)
+										end (line-end-position)))
       (if mark-active
-	  (setq begin (region-beginning)
-		end (region-end))
-	(setq begin (line-beginning-position)
-	      end (line-end-position))))
+					(setq begin (region-beginning)
+								end (region-end))
+				(setq begin (line-beginning-position)
+							end (line-end-position))))
     (comment-or-uncomment-region begin end)
     (if-fn% next-logical-line nil
-	    (next-logical-line)
+						(next-logical-line)
       (next-line))))
 
 ;; toggle comment key strike1
