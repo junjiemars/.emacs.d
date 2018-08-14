0004e6396851e6299740344118698142a8a25bc9
南山 <junjiemars@gmail.com>
2018-08-14 23:58:06 +0800

    :desktop does not save tramp file

--- config/sample-self-env-spec.el
+++ config/sample-self-env-spec.el
@@ -45,7 +45,7 @@
                :allowed nil)
   
   :desktop (list :files-not-to-save
-                 ".*\.t?gz\\|\.desktop\\|~$\\|\\/ssh[: ]\\|\.elc$"
+                 ".*\.t?gz\\|\.desktop\\|~$\\|^/ssh[x]?:\\|\.elc$"
                  :buffers-not-to-save "^TAGS\\|\\.log"
                  :modes-not-to-save
                  '(dired-mode fundamental-mode eww-mode rmail-mode)
