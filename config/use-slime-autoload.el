b1e46abe8219aff368ece89a6a9912272914029e
南山竹 <junjiemars@gmail.com>
2018-08-28 15:23:34 +0800

    deprecated global-set-key in use-slime-autoload.el

--- config/use-slime-autoload.el
+++ config/use-slime-autoload.el
@@ -6,11 +6,14 @@
 
 
 (with-eval-after-load 'slime
+
   (when-fn% set-slime-lisp-implementations! use-slime
     (require 'use-slime)
     (set-slime-lisp-implementations!))
+
   (when-fn% slime-setup slime
     (slime-setup '(slime-fancy slime-asdf)))
+
   (when-fn% slime-selector slime
-    (global-set-key (kbd "C-c s s") #'slime-selector)))
+    (define-key (current-global-map) (kbd "C-c s s") #'slime-selector)))
 
