a8437687378438ae82a1b9fbe8258638a0c31e65
南山竹 <junjiemars@gmail.com>
2018-10-08 00:34:04 +0800

    untabify

--- config/use-slime.el
+++ config/use-slime.el
@@ -9,29 +9,29 @@
 'sbcl
 '\(ecl :init slime-init-comman\)
 '\(acl \(\"acl7\" \"-quiet\"\)
-			:coding-system emacs-mule\)"
+      :coding-system emacs-mule\)"
   (declare (indent 0))
   `(let ((lisps nil))
      (dolist (x (list ,@lispspec) lisps)
        (let ((lisp (if (consp x) (car x) x)))
-	 (when (executable-find (symbol-name lisp))
-	   (if (consp x)
-	       (if (consp (cadr x))
-		   (push x lisps)
-		 (push (list lisp
-			     (append
-			      (list (executable-find (symbol-name lisp)))
-			      (cdr x)))
-		       lisps))
-	     (push (list lisp
-			 (list (executable-find (symbol-name lisp))))
-		   lisps)))))))
+         (when (executable-find (symbol-name lisp))
+           (if (consp x)
+               (if (consp (cadr x))
+                   (push x lisps)
+                 (push (list lisp
+                             (append
+                              (list (executable-find (symbol-name lisp)))
+                              (cdr x)))
+                       lisps))
+             (push (list lisp
+                         (list (executable-find (symbol-name lisp))))
+                   lisps)))))))
 
 (defun set-slime-lisp-implementations! ()
   "More easy way to set `slime-lisp-implementations'."
   (setq% slime-lisp-implementations
-	 (common-lisp-implementations 'acl 'ccl 'clasp 'ecl 'sbcl)
-	 slime))
+         (common-lisp-implementations 'acl 'ccl 'clasp 'ecl 'sbcl)
+         slime))
 
 
 (provide 'use-slime)
