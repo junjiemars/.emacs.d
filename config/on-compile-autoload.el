d5fc07166bf79b12f34c80650be52e51f63adbbc
南山竹 <junjiemars@gmail.com>
2018-10-08 00:17:00 +0800

    untabify

--- config/on-compile-autoload.el
+++ config/on-compile-autoload.el
@@ -13,9 +13,9 @@
   ;; When such case occurred, we try to translate UNIX path to POSIX path.
   (defadvice compilation-find-file (before compilation-find-file-before compile)
     (when (string-match "^/\\([a-zA-Z]\\)/" (ad-get-arg 1))
-			(ad-set-arg 1 ;; filename argument
-									(replace-match (concat (match-string 1 (ad-get-arg 1)) ":/")
-																 t t (ad-get-arg 1))))))
+      (ad-set-arg 1 ;; filename argument
+                  (replace-match (concat (match-string 1 (ad-get-arg 1)) ":/")
+                                 t t (ad-get-arg 1))))))
 
 
 (with-eval-after-load 'compile
@@ -25,9 +25,9 @@
     (ad-activate #'compilation-find-file t))
   
   (add-hook 'compilation-filter-hook #'colorize-compilation-buffer!)
-	(when-var% compilation-mode-map compile
-						 ;; define `recompile' and `quit-window' key bindings
-						 (define-key% compilation-mode-map (kbd "g") #'recompile)
-						 (define-key% compilation-mode-map (kbd "q") #'quit-window))
+  (when-var% compilation-mode-map compile
+    ;; define `recompile' and `quit-window' key bindings
+    (define-key% compilation-mode-map (kbd "g") #'recompile)
+    (define-key% compilation-mode-map (kbd "q") #'quit-window))
   (setq% compilation-scroll-output t compile))
 
