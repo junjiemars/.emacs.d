1fd9ebae3cd603d8d285d239bf6a2cd2bb894e7e
南山竹 <junjiemars@gmail.com>
2019-01-13 14:43:56 +0800

    fix on-module.el loaded twice

--- config/strap.el
+++ config/strap.el
@@ -430,8 +430,7 @@ Take effect after restart Emacs.
 
 
 ;; Load package independent modules
-(compile! (compile-unit% (emacs-home* "config/on-module.el"))
-          (compile-unit% (emacs-home* "config/eshells.el"))
+(compile! (compile-unit% (emacs-home* "config/eshells.el"))
           (compile-unit% (emacs-home* "config/autoload.el"))
           ;; --batch mode: disable desktop read/save
           `,(unless noninteractive 
