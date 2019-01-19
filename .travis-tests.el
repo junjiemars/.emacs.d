01100d4502cced5e67539e947415b8c8a99d1c42
南山竹 <junjiemars@gmail.com>
2019-01-19 21:45:52 +0800

    fix issue: .travis-test.el

--- .travis-tests.el
+++ .travis-tests.el
@@ -18,11 +18,6 @@
   (should-not (comment (+ 1 2 3)))
   (should-not (comment (progn (+ 1) (* 2 3)))))
 
-(ert-deftest %init:+emacs-home+ ()
-  (if (boundp 'user-emacs-directory)
-      (should (string= +emacs-home+ (expand-file-name user-emacs-directory)))
-    (should (string= +emacs-home+ (expand-file-name "~/.emacs.d/")))))
-
 (ert-deftest %init:emacs-home* ()
   (should (string-match "\.emacs\.d/$" (emacs-home*)))
   (should (string-match "\.emacs\.d/config/$" (emacs-home* "config/")))
