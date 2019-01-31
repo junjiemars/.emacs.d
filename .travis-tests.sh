ff922bd5658d3a1c2b9da440b8ce62feddae5599
南山竹 <junjiemars@gmail.com>
2019-01-31 23:01:34 +0800

    just clean, compile and loading

--- .travis-tests.sh
+++ .travis-tests.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
+ROOT="`cd $(dirname $0) && pwd`"
+EMACS="${EMACS:-emacs}"
+
 # emacs --batch \
 # 			-l init.el \
 # 			-l .travis-tests.el \
 # 			-f ert-run-tests-batch-and-exit
 
 emacs --batch \
-			--load=./init.el \
+			--load="${ROOT}/init.el" \
 			--eval='(clean-compiled-files)'
+
+emacs --batch \
+			--load="${ROOT}/init.el"
 
