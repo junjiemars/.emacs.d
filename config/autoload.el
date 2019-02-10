3fe447377aff2cce968e8c9bc3fcbfb04bec27ed
南山竹 <junjiemars@gmail.com>
2019-02-10 20:22:29 +0800

    autoload jump.el

--- config/autoload.el
+++ config/autoload.el
@@ -59,6 +59,7 @@
 
   (compile!
     (compile-unit% (emacs-home* "config/financial.el") t)
+    (compile-unit% (emacs-home* "config/jump.el") t)
     (compile-unit% (emacs-home* "config/tags.el") t)
     (compile-unit% (emacs-home* "config/on-cc-autoload.el"))
     (compile-unit% (emacs-home* "config/on-compile-autoload.el"))
