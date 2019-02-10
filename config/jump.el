2d39b57f0b73a042a3f618b8ef0682121b683161
南山竹 <junjiemars@gmail.com>
2019-02-10 20:22:08 +0800

    jump.el

---
+++ config/jump.el
@@ -0,0 +1,13 @@
+;;;; -*- lexical-binding:t -*-
+;;;;
+;; More reasonable Emacs on MacOS, Windows and Linux
+;; https://github.com/junjiemars/.emacs.d
+;;;;
+;; jump.el
+;;;;
+
+
+
+
+
+;; end of file
