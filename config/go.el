8b67faab27c356132893554cb36e1a54b92375a4
南山竹 <junjiemars@gmail.com>
2019-02-17 13:55:27 +0800

    prefer-project-root fn

--- config/go.el
+++ config/go.el
@@ -22,18 +22,28 @@
                            (line-end-position)))))
 
 (defun find-project-root (file)
-  "Return a list of possible root via look up directories of FILE."
+  "Return a alist of possible root via look up directories of FILE."
   (let ((found nil))
     (catch 'out
-      (dir-backtrack (file-name-directory file)
-                     (lambda (d fs)
-                       (dolist* (f fs)
-                         (cond ((string-match "[mM]akefile\\'" f)
-                                (push (concat d f) found))
-                               ((string-match "/\\.git/\\'\\|/\\.svn/\\'"
-                                              (concat d f))
-                                (throw 'out
-                                       (push (concat d f) found))))))))))
+      (dir-backtrack
+       (file-name-directory file)
+       (lambda (d fs)
+         (dolist* (f fs)
+           (cond ((string= "Makefile" f)
+                  (push (list 'makefile (concat d f)) found))
+                 ((string= ".git/" f)
+                  (throw 'out (push (list 'git (concat d f)) found)))
+                 ((string= ".svn/" f)
+                  (throw 'out (push (list 'svn (concat d f)) found))))))))))
+
+(defun prefer-project-root (seq &optional prefer)
+  "Prefer what as project root."
+  (let ((prefer (or prefer '("git" "svn"))))
+    (catch 'out
+      (dolist* (x prefer)
+        (let ((found (alist-get* x seq nil nil #'string=)))
+          (when found (throw 'out (car found))))))))
+
 
 (provide 'go)
 
