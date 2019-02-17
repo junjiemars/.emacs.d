1173dad20353185a270fb0737cadb1c1f0af3ea5
南山竹 <junjiemars@gmail.com>
2019-02-17 15:37:09 +0800

    format

--- config/on-compile-autoload.el
+++ config/on-compile-autoload.el
@@ -11,7 +11,8 @@
   ;; There are no builtin `grep' in Windows, GNU's `grep' may use
   ;; the UNIX path in Windows which cannot be recognized by Emacs.
   ;; When such case occurred, we try to translate UNIX path to POSIX path.
-  (defadvice compilation-find-file (before compilation-find-file-before compile)
+  (defadvice compilation-find-file
+      (before compilation-find-file-before compile)
     (when (string-match "^/\\([a-zA-Z]\\)/" (ad-get-arg 1))
       (ad-set-arg 1 ;; filename argument
                   (replace-match (concat (match-string 1 (ad-get-arg 1)) ":/")
