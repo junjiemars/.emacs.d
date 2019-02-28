3e461aaaecde207e9888fd5cba892e621263d380
南山竹 <junjiemars@gmail.com>
2019-02-28 21:20:07 +0800

    merge on-hippie-autoload.el into on-edit-autoload.el

--- config/autoload.el
+++ config/autoload.el
@@ -59,7 +59,6 @@
     (compile-unit% (emacs-home* "config/on-compile-autoload.el"))
     (compile-unit% (emacs-home* "config/on-dired-autoload.el"))
     (compile-unit% (emacs-home* "config/on-edit-autoload.el"))
-    (compile-unit% (emacs-home* "config/on-hippie-autoload.el"))
     (compile-unit% (emacs-home* "config/on-lisp-autoload.el"))
 		(compile-unit% (emacs-home* "config/on-python-autoload.el"))
     (compile-unit% (emacs-home* "config/on-sh-autoload.el"))
