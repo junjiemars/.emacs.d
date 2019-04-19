0004640caa0b88efc20d1d831639750c85839bea
南山竹 <junjiemars@gmail.com>
2019-04-19 10:15:51 +0800

    rename

--- config/on-eshell-autoload.el
+++ config/on-eshell-autoload.el
@@ -7,25 +7,26 @@
 ;;;;
 
 
-(defun set-eshell-mode! ()
+(defmacro self-eshell-mode! ()
   "Set the basics of `eshell-mode'"
-  (eval-when-compile (require 'em-term))
-  (require 'em-term)
-  (when (self-spec->*env-spec :eshell :allowed)
-    (mapc (lambda (x)
-            (add-to-list 'eshell-visual-commands x t #'string=))
-          (self-spec->*env-spec :eshell :visual-commands))
-    (setq% eshell-destroy-buffer-when-process-dies
-           (self-spec->*env-spec
-             :eshell :destroy-buffer-when-process-dies))
-    (setq% eshell-visual-subcommands
-           (self-spec->*env-spec :eshell :visual-subcommands))
-    (setq% eshell-visual-options
-           (self-spec->*env-spec :eshell :visual-options))))
+  `(progn
+     (eval-when-compile (require 'em-term))
+     (require 'em-term)
+     (when (self-spec->*env-spec :eshell :allowed)
+       (mapc (lambda (x)
+               (add-to-list 'eshell-visual-commands x t #'string=))
+             (self-spec->*env-spec :eshell :visual-commands))
+       (setq% eshell-destroy-buffer-when-process-dies
+              (self-spec->*env-spec
+                :eshell :destroy-buffer-when-process-dies))
+       (setq% eshell-visual-subcommands
+              (self-spec->*env-spec :eshell :visual-subcommands))
+       (setq% eshell-visual-options
+              (self-spec->*env-spec :eshell :visual-options)))))
 
 
 (with-eval-after-load 'eshell
-  (set-eshell-mode!)
+  (self-eshell-mode!)
 
   ;; abbreviated `eshell' prompt
   (version-supported-when > 23
