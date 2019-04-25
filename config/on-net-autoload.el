dadcba016057a718d214a392a34798f452974178
南山竹 <junjiemars@gmail.com>
2019-04-25 19:44:35 +0800

    refine on-net-autoload.el

--- config/on-net-autoload.el
+++ config/on-net-autoload.el
@@ -7,46 +7,50 @@
 ;;;;
 
 
-(defun *arp (&optional arg)
-  "Run `arp-program' and display diagnostic output."
-  (interactive (when current-prefix-arg
-                 (list (read-from-minibuffer "Arp options: "))))
-  (when-fn% 'arp 'net-utils
+(when-fn% 'arp 'net-utils
+  (defun *arp (&optional arg)
+    "Run `arp-program' and display diagnostic output."
+    (interactive (when current-prefix-arg
+                   (list (read-from-minibuffer "Arp options: "))))
     (require 'net-utils)
     (let ((arp-program-options (if arg (split-string* arg " " t)
                                  arp-program-options)))
       (arp))))
 
 
-(defun *ifconfig (&optional arg)
-  "Run `ifconfig-program' and display diagnostic output."
-  (interactive (when current-prefix-arg
-                 (list (read-from-minibuffer "Ifconfig options: "))))
-  (when-fn% 'ifconfig 'net-utils
+(when-fn% 'ifconfig 'net-utils
+  (defun *ifconfig (&optional arg)
+    "Run `ifconfig-program' and display diagnostic output."
+    (interactive (when current-prefix-arg
+                   (list (read-from-minibuffer "Ifconfig options: "))))
     (require 'net-utils)
-    (let ((ifconfig-program-options (if arg (split-string* arg " " t)
-                                      ifconfig-program-options)))
-      (ifconfig))))
+    (if-var% ifconfig-program-options 'net-utils
+             (let ((ifconfig-program-options (if arg (split-string* arg " " t)
+                                               ifconfig-program-options)))
+               (ifconfig))
+      (let ((ipconfig-program-options (if arg (split-string* arg " " t)
+                                        ipconfig-program-options)))
+        (ipconfig)))))
 
 
-(defun *netstat (&optional arg)
-  "Run `netstat-program' and display diagnostic output."
-  (interactive (when current-prefix-arg
-                 (list (read-from-minibuffer "Netstat options: "))))
-  (when-fn% 'netstat 'net-utils
+(when-fn% 'netstat 'net-utils
+  (defun *netstat (&optional arg)
+    "Run `netstat-program' and display diagnostic output."
+    (interactive (when current-prefix-arg
+                   (list (read-from-minibuffer "Netstat options: "))))
     (require 'net-utils)
     (let ((netstat-program-options (if arg (split-string* arg " " t)
                                      netstat-program-options)))
       (netstat))))
 
 
-(defun *ping (&optional arg)
-  "Run `ping-program' ping host."
-  (interactive (when current-prefix-arg
-                 (list (read-from-minibuffer "Ping options: "))))
-  (when-fn% 'netstat 'net-utils
+(when-fn% 'netstat 'net-utils
+  (defun *ping (&optional arg)
+    "Run `ping-program' ping host."
+    (interactive (when current-prefix-arg
+                   (list (read-from-minibuffer "Ping options: "))))
     (require 'net-utils)
     (let ((ping-program-options (if arg (split-string* arg " " t)
                                   ping-program-options)))
-      (call-interactively #'ping))))
+      (call-interactively #'ping t))))
 
