5b3e29601e5e0c246720419594b9694dcb9222f3
南山竹 <junjiemars@gmail.com>
2019-05-18 15:00:06 +0800

    use-org-autoload.el

---
+++ config/use-org-autoload.el
@@ -0,0 +1,19 @@
+;;;; -*- lexical-binding:t -*-
+;;;;
+;; More reasonable Emacs on MacOS, Windows and Linux
+;; https://github.com/junjiemars/.emacs.d
+;;;;
+;; use-org-autoload.el (Lisp Flavoured Erlang)
+;;;;
+
+
+(defmacro-if-feature% ox-reveal)
+
+(with-eval-after-load 'org
+
+  ;; load `ox-reveal' if it had been installed.
+  (if-feature-ox-reveal%
+    (require 'ox-reveal)))
+
+
+
