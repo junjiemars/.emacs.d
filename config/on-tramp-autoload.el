f332f1666fc6647b377310d023072449a2ef70fc
南山竹 <junjiemars@gmail.com>
2019-06-19 10:49:17 +0800

    new docker-containers fn

--- config/on-tramp-autoload.el
+++ config/on-tramp-autoload.el
@@ -7,8 +7,14 @@
 ;;;;
 
 
-(when% (and (executable-find% "docker")
-            (require 'tramp)
+(defun docker-containers ()
+  "Return a list of running docker container."
+  (let ((cmd (shell-command* "docker" "ps" "--format {{.Names}}")))
+    (when (zerop (car cmd))
+      (split-string* (cdr cmd) "\n" t "\n"))))
+
+
+(when% (and (require 'tramp)
             (not (find "docker" tramp-methods :key #'car :test #'string=)))
   (with-eval-after-load 'tramp
     (add-to-list 'tramp-methods
