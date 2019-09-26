2e8ed3daa117fef8aa7f6016b0037d1a23aa90f0
南山竹 <junjiemars@gmail.com>
2019-09-26 15:10:26 +0800

    encode-url on region

--- config/on-edit-autoload.el
+++ config/on-edit-autoload.el
@@ -507,4 +507,23 @@ directory name of `buffer-file-name' to kill ring."
                (car (occur-read-primary-args))))
 
 
+(defun encode-url (&optional arg)
+  "Encode the current region into *encode-url-output* buffer.
+
+If ARG is non nil then decode the current region into
+*decode-url-output* buffer."
+  (interactive "P")
+  (let ((s (region-active-if
+               (buffer-substring (region-beginning)
+                                 (region-end)))))
+    (with-current-buffer
+        (switch-to-buffer-other-window
+         (if arg "*decode-url-output*"
+           "*encode-url-output*"))
+      (delete-region (point-min) (point-max))
+      (insert (if arg
+                  (decode-coding-string (url-unhex-string s) 'utf-8)
+                (url-hexify-string s))))))
+
+
 ;; end of file
