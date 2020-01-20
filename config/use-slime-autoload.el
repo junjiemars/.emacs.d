0c9f9efc99c51fe0e1405caea2ad30facef6e8d4
南山竹 <junjiemars@gmail.com>
2020-01-20 22:47:22 +0800

    refine set-slime-lisp-implementations!

--- config/use-slime-autoload.el
+++ config/use-slime-autoload.el
@@ -7,36 +7,14 @@
 ;;;;
 
 
-(eval-when-compile
-
-  (defmacro _common_lisp_implementations_ (&rest lispspec)
-    "LISPSPEC such as:
-'sbcl
-'(ecl :init slime-init-command)
-'(acl (\"acl7\" \"-quiet\")
-      :coding-system emacs-mule)"
-    (declare (indent 0))
-    `(let ((lisps nil))
-       (dolist* (x (list ,@lispspec) lisps)
-         (let* ((lisp (if (consp x) (car x) x))
-                (bin (executable-find (symbol-name lisp))))
-           (when bin
-             (if (consp x)
-                 (if (consp (cadr x))
-                     (push x lisps)
-                   (push (list lisp (append (list bin) (cdr x))) lisps))
-               (push (list lisp (list bin)) lisps))))))))
-
-
 (defun set-slime-lisp-implementations! ()
   "More easy way to set `slime-lisp-implementations'."
   (setq% slime-lisp-implementations
-         (_common_lisp_implementations_
-          'acl
-          'ccl
-          'clasp
-          'ecl
-          'sbcl)
+         (let ((impls nil))
+           (dolist* (x '(acl ccl clasp ecl sbcl) impls)
+             (let* ((impl (if (consp x) (car x) x))
+                    (bin (executable-find (symbol-name impl))))
+               (when bin (push (list impl (list bin)) impls)))))
          'slime))
 
 
