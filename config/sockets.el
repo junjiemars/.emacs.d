957649945200b5ed8b40ca88f8af4463c3424bc6
南山竹 <junjiemars@gmail.com>
2020-02-07 21:58:55 +0800

    fix url-gateway-method bug above emacs 25

--- config/sockets.el
+++ config/sockets.el
@@ -9,51 +9,60 @@
 
 (defmacro-if-feature% socks)
 
-(comment
- (and (require 'url-vars nil t)
-      (require 'socks nil t)
-      (boundp 'url-gateway-method)
-      (boundp 'socks-server)))
 
 (if-feature-socks%
- 
- (defun toggle-socks! (&optional arg)
-   "Toggle `url-gatewary-method' to socks or native.
+
+    (defun toggle-socks! (&optional arg)
+      "Toggle `url-gatewary-method' to socks or native.
 With prefix argument ARG, `url-gatewary-method' via socks if ARG is 
 positive, otherwise via native."
-   (interactive "P")
-   (let ((native (lambda ()
-                   (setq% url-gateway-method 'native 'url-vars)
-                   (setq% socks-server
-                          (list "Default server"
-                                nil
-                                (self-spec->*env-spec :socks :port)
-                                (self-spec->*env-spec :socks :version))
-                          'socks)))
-         (socks (lambda ()
-                  (setq% url-gateway-method 'socks 'url-vars)
-                  (setq% socks-server
-                         (list "Default server"
-                               (self-spec->*env-spec :socks :server)
-                               (self-spec->*env-spec :socks :port)
-                               (self-spec->*env-spec :socks :version))
-                         'socks))))
-     ;; (require 'url)
-     (if (null arg)
-         (if (eq url-gateway-method 'native) (funcall socks) (funcall native))
-       (funcall socks))
-     (message "socks%s as url gateway %s"
-              (list (self-spec->*env-spec :socks :server)
-                    (self-spec->*env-spec :socks :port)
-                    (self-spec->*env-spec :socks :version))
-              (if (eq url-gateway-method 'native)
-                  "disabled"
-                "enabled")))))
+      (interactive "P")
+      (let ((native (lambda ()
+                      (setq% url-gateway-method 'native 'url-vars)
+                      (setq% socks-server
+                             (list "Default server"
+                                   nil
+                                   (self-spec->*env-spec :socks :port)
+                                   (self-spec->*env-spec :socks :version))
+                             'socks)))
+            (socks (lambda ()
+                     (setq% url-gateway-method 'socks 'url-vars)
+                     (setq% socks-server
+                            (list "Default server"
+                                  (self-spec->*env-spec :socks :server)
+                                  (self-spec->*env-spec :socks :port)
+                                  (self-spec->*env-spec :socks :version))
+                            'socks))))
+        ;; (require 'url)
+        (if (null arg)
+            (if (eq url-gateway-method 'native) (funcall socks) (funcall native))
+          (funcall socks))
+        (message "socks%s as url gateway %s"
+                 (list (self-spec->*env-spec :socks :server)
+                       (self-spec->*env-spec :socks :port)
+                       (self-spec->*env-spec :socks :version))
+                 (if (eq url-gateway-method 'native)
+                     "disabled"
+                   "enabled")))))
+
+
+(when-version% <= 25
+
+  (defadvice url-http (before url-http-before compile)
+    "Fix the `url-gateway-method' bug in `url-http'."
+    (ad-set-arg 4 (let ((gateway-method (ad-get-arg 4)))
+                    (cond ((null gateway-method) url-gateway-method)
+                          ((and (eq 'socks url-gateway-method)
+                                (not (eq gateway-method url-gateway-method)))
+                           url-gateway-method)
+                          (t gateway-method))))))
 
 
 (if-feature-socks%
- (when (self-spec->*env-spec :socks :allowed)
-   (toggle-socks! t)))
+    (when (self-spec->*env-spec :socks :allowed)
+      (when-version% <= 25
+        (ad-activate #'url-http t))
+      (toggle-socks! t)))
 
 
-;; end of file
+;; end of sockets.el file
