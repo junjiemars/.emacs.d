107e4a856af85be91e6dff8e8d342ebd250dc16a
南山竹 <junjiemars@gmail.com>
2020-03-04 20:28:41 +0800

    just one key for magit-status
    it's enough

--- config/use-magit-autoload.el
+++ config/use-magit-autoload.el
@@ -12,43 +12,12 @@
       ;; On Windows try to open remote git repo via sshx
       ;; will trigger `magit' error: No such file or directory.
       ;; GitHub issue: https://github.com/magit/magit/issues/3345
-      (setq% magit-git-executable "git" 'magit)))
+      (setq% magit-git-executable "git" 'magit))))
 
-  ;;
-  ;; define keys for `magit'
-  ;;
-  (define-key% (current-global-map) (kbd "C-c v p")
-    (if-fn% 'magit-pull-branch 'magit
-            #'magit-pull-branch
-      #'magit-pull))
-
-  (define-key% (current-global-map) (kbd "C-c v P")
-    (if-fn% 'magit-push-other 'magit
-            'magit-push-other
-      #'magit-push))
-
-  (define-key% (current-global-map) (kbd "C-c v l")
-    (if-fn% #'magit-log-other 'magit
-            #'magit-log-other
-      #'magit-log))
-
-  (define-key% (current-global-map) (kbd "C-c v b")
-    #'magit-log-buffer-file)
-
-  (define-key% (current-global-map) (kbd "C-c v c") #'magit-checkout)
-
-  (define-key% (current-global-map) (kbd "C-c v m")
-    (if-fn% #'magit-merge-plain 'magit
-            #'magit-merge-plain
-      #'magit-merge))
-  
-  (define-key% (current-global-map) (kbd "C-c v f")
-    (if-fn% #'magit-fetch-other 'magit
-            #'magit-fetch-other
-      #'magit-fetch)))
 
 (when-fn% 'magit-status 'magit
-  (define-key (current-global-map) (kbd "C-c v s")
+  ;; should take up the `vc-create-tag' key binding
+  (define-key (current-global-map) (kbd "C-x v s")
     #'magit-status))
 
 
