9a9f6c1a10f32fa724e1c93c06543cb8189eb46a
南山竹 <junjiemars@gmail.com>
2020-03-08 14:49:29 +0800

    new dicts: extract via regexp

--- config/dicts.el
+++ config/dicts.el
@@ -13,10 +13,14 @@
 
 
 (defvar *dicts*
-  '(("bing" (:url "http://www.bing.com/dict/search?mkt=zh-cn&q="
-                  :us-pronounce
-                  "<div class=\"hd_prUS b_primtxt\">\\(.+\\)</div>"
-                  :area (""))))
+  '(("bing"
+     (url . "http://www.bing.com/dict/search?mkt=zh-cn&q=")
+     (pron-us . ("<div class=\"hd_prUS b_primtxt\">"
+                 .
+                 ".+[^<]"))
+     (pron-uk . ("<div class=\"hd_pr b_primtxt\">"
+                 .
+                 ".+[^<]"))))
   "Dictionaries using by `lookup-dict'.")
 
 
@@ -25,32 +29,59 @@
   "Callback when `lookup-dict'."
   (declare (indent 1))
   (set-buffer-multibyte t)
-  (write-region (point-min) (point-max) (emacs-home* "private/dict.txt"))
-  
-  )
+  ;; (write-region (point-min) (point-max) (emacs-home* "private/dict.txt"))
+  (let ((dict (remove-if* (lambda (x)
+                            (eq 'url (car x)))
+                  (cdadr (assoc** 'dict args #'eq))))
+        (style (cdr (assoc** 'style args #'eq)))
+        (ss nil))
+    (dolist* (x style (nreverse ss))
+      (goto-char (point-min))
+      (let* ((re (cdr (assoc** x dict #'eq)))
+             (b (re-search-forward (car re) nil t))
+             (e (when b (re-search-forward (cdr re) nil t))))
+        (when (and b (< b e))
+          (push (cons x (buffer-substring-no-properties b e))
+                ss))))))
 
 
-(defun lookup-dict (word &optional dict)
+(defun lookup-dict (what &optional dict)
   "Show the explanation of WORD from DICT in the echo area."
   (interactive
    (list (read-string "lookup dict for: " (cdr (symbol@)))
          (when current-prefix-arg
-           (read-string (format "Choose (%s): "
-                                (mapconcat
-                                 #'identity
-                                 (mapcar #'car *dicts*)
-                                 "|"))))))
-  (let* ((d1 (if (or (null dict)
-                     (string= "" dict))
-                 "bing"
+           (let* ((n (mapconcat #'identity
+                                (mapcar #'car *dicts*)
+                                "|"))
+                  (d (read-string (format "Choose (%s): " n) n))
+                  (dd (assoc** d *dicts* #'string=))
+                  (s (mapcar (lambda (x)
+                               (symbol-name (car x)))
+                             (cdr dd)))
+                  (sr (remove-if* (lambda (x)
+                                    (string= "url" x))
+                          s))
+                  (ss (read-string (format "Choose (all|%s): "
+                                           (mapconcat #'identity
+                                                      sr
+                                                      ",")))))
+             `((dict . ,dd)
+               (style . ,(if (string= "all" ss)
+                             sr
+                           ss)))))))
+  (let* ((d1 (if (null dict)
+                 `((dict ,(car *dicts*))
+                   (style ,(remove-if* (lambda (x)
+                                         (eq 'url x))
+                               (mapcar #'car
+                                       (cdr (car *dicts*))))))
                dict))
-         (d2 (cadr (assoc** d1 *dicts* #'string=)))
-         (url (plist-get d2 :url)))
-    (url-retrieve (concat url (url-hexify-string word))
+         (url (cdr (assoc** 'url (cdadr (assoc** 'dict d1 #'eq)) #'eq))))
+    (url-retrieve (concat url (url-hexify-string what))
                   #'on-lookup-dict
-                  `(:word ,(decode-coding-string word 'utf-8)
-                          :dict ,d2
-                          :style nil)
+                  `((what . ,(decode-coding-string what 'utf-8))
+                    (dict . ,(cdr (assoc** 'dict d1 #'eq)))
+                    (style . ,(cadr (assoc** 'style d1 #'eq))))
                   t
                   t)))
 
