a9a785ce8449724bb38d60d9f49da8af6cd54f37
南山竹 <junjiemars@gmail.com>
2020-03-28 16:33:22 +0800

    process-send-string

--- config/on-term-autoload.el
+++ config/on-term-autoload.el
@@ -13,24 +13,23 @@
   (let ((proc (get-buffer-process (current-buffer))))
     (unless proc
       (error "Current buffer has no process"))
-    (when-fn% 'term-send-string 'term
-      (let ((zsh (or (and (self-spec->*env-spec :shell :allowed)
-                          (self-spec->*env-spec :shell :shell-prompt :zsh))
-                     "%n@%m:%~ %# "))
-            (oth (or (and (self-spec->*env-spec :shell :allowed)
-                          (self-spec->*env-spec :shell :shell-prompt :bash))
-                     "\\u@\\h:\\w\\$ ")))
-        (term-send-string proc
-                          (concat "case \"`basename $SHELL`\" in\n"
-                                  "  zsh)\n"
-                                  (format "    export PS1='%s'\n" zsh)
-                                  "export PROMPT_COMMAND=''\n"
-                                  "    ;;\n"
-                                  "  *)\n"
-                                  (format "    export PS1='%s'\n" oth)
-                                  "export PROMPT_COMMAND=''\n"
-                                  "    ;;\n"
-                                  "esac\n"))))))
+    (let ((zsh (or (and (self-spec->*env-spec :shell :allowed)
+                        (self-spec->*env-spec :shell :shell-prompt :zsh))
+                   "%n@%m:%~ %# "))
+          (oth (or (and (self-spec->*env-spec :shell :allowed)
+                        (self-spec->*env-spec :shell :shell-prompt :bash))
+                   "\\u@\\h:\\w\\$ ")))
+      (process-send-string proc
+                           (concat "case \"`basename $SHELL`\" in\n"
+                                   "  zsh)\n"
+                                   (format "    export PS1='%s'\n" zsh)
+                                   "export PROMPT_COMMAND=''\n"
+                                   "    ;;\n"
+                                   "  *)\n"
+                                   (format "    export PS1='%s'\n" oth)
+                                   "export PROMPT_COMMAND=''\n"
+                                   "    ;;\n"
+                                   "esac\n")))))
 (with-eval-after-load 'term
   (define-key term-mode-map (kbd "C-c t p") #'term-unify-shell-prompt))
 
