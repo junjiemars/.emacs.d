5ecbdbd57267c60d0db7aa98d5a4deb69b6b41a8
南山竹 <junjiemars@gmail.com>
2020-04-19 13:18:08 +0800

    word, quoted string, filename isearching

--- config/on-isearch-autoload.el
+++ config/on-isearch-autoload.el
@@ -7,26 +7,59 @@
 ;;;;
 
 
-(defun isearch-forward* (&optional arg)
+(defun isearch-forward* (&optional regexp-p)
   "Do incremental region or symbol search forward."
   (interactive "P")
-  (isearch-forward arg 1)
+  (isearch-forward regexp-p 1)
   (let ((ss (symbol@)))
     (when (eq 'region (car ss))
       (isearch-yank-string (cdr ss)))))
 
-(defun isearch-backward* (&optional arg)
+(defun isearch-backward* (&optional regexp-p)
   "Do incremental region or symbol search backward."
   (interactive "P")
-  (isearch-backward arg 1)
+  (isearch-backward regexp-p 1)
   (let ((ss (symbol@)))
     (when (eq 'region (car ss))
       (isearch-yank-string (cdr ss)))))
+
+(defun isearch-forward-word* ()
+  "Do incremental word forward."
+  (interactive)
+  (if (mark-word@)
+      (isearch-forward*)
+    (message "%s: [No word at point]"
+             (propertize "Word I-search"
+                         'face 'minibuffer-prompt))))
+
+(defun isearch-forward-string* ()
+  "Do incremental string forward."
+  (interactive)
+  (if (mark-string@)
+      (isearch-forward*)
+    (message "%s: [No string at point]"
+             (propertize "String I-search"
+                         'face 'minibuffer-prompt))))
+
+(defun isearch-forward-filename* ()
+  "Do incremental filename backward."
+  (interactive)
+  (if (mark-filename@)
+      (isearch-forward*)
+    (message "%s: [No filename at point]"
+             (propertize "Filename I-search"
+                         'face 'minibuffer-prompt))))
 
 
 (with-eval-after-load 'isearch
   (define-key% (current-global-map) (kbd "C-s") #'isearch-forward*)
-  (define-key% (current-global-map) (kbd "C-r") #'isearch-backward*))
+  (define-key% (current-global-map) (kbd "C-r") #'isearch-backward*)
+  (define-key% (current-global-map)
+    (kbd "M-s w") #'isearch-forward-word*)
+  (define-key% (current-global-map)
+    (kbd "M-s q") #'isearch-forward-string*)
+  (define-key% (current-global-map)
+    (kbd "M-s f") #'isearch-forward-filename*))
 
 
  ;; end of file
