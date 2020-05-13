3b627aa8dcac8847048bd390fceb1cc160bc7b9f
南山竹 <junjiemars@gmail.com>
2020-05-14 01:10:21 +0800

    isearch-forward*/backward*

--- config/on-isearch-autoload.el
+++ config/on-isearch-autoload.el
@@ -7,15 +7,7 @@
 ;;;;
 
 
-(defun isearch-forward* (&optional regexp-p)
-  "Do incremental search forward."
-  (interactive "P")
-  (isearch-forward regexp-p 1)
-  (let ((ss (symbol@)))
-    (when (eq 'region (car ss))
-      (isearch-yank-string (cdr ss)))))
-
-(defun isearch-forward*1 (&optional style backward)
+(defun isearch-forward* (&optional style backward)
   "Do incremental search forward."
   (interactive
    (list (when current-prefix-arg
@@ -43,87 +35,19 @@
                                'face 'font-lock-warning-face)))))))
 
 
-(defun isearch-backward*1 (&optional style)
+(defun isearch-backward* (&optional style)
   "Do incremental search backward."
   (interactive
    (list (when current-prefix-arg
            (read-key (format
                       "Choose I-search style: %s "
                       "(r)egexp (s)ymbol (w)ord (f)ile (q)uoted")))))
-  (isearch-forward*1 style t)
-  )
-
-
-(defun isearch-backward* (&optional regexp-p)
-  "Do incremental search backward."
-  (interactive "P")
-  (isearch-backward regexp-p 1)
-  (let ((ss (symbol@)))
-    (when (eq 'region (car ss))
-      (isearch-yank-string (cdr ss)))))
-
-
-(defun isearch-forward-symbol* (&optional arg)
-  "Do incremental symbol forward."
-  (interactive "P")
-  (if (mark-symbol@)
-      (if arg
-          (isearch-backward*)
-        (isearch-forward*))
-    (comment arg)
-    (message "%s: [No symbol at point]"
-             (propertize "Symbol I-search"
-                         'face 'minibuffer-prompt))))
-
-
-(defun isearch-forward-word* (&optional arg)
-  "Do incremental word forward."
-  (interactive "P")
-  (if (mark-word@)
-      (if arg
-          (isearch-backward*)
-        (isearch-forward*))
-    (comment arg)
-    (message "%s: [No word at point]"
-             (propertize "Word I-search"
-                         'face 'minibuffer-prompt))))
-
-(defun isearch-forward-string* (&optional arg)
-  "Do incremental string forward."
-  (interactive "P")
-  (if (mark-string@)
-      (if arg
-          (isearch-backward*)
-        (isearch-forward*))
-    (comment arg)
-    (message "%s: [No string at point]"
-             (propertize "String I-search"
-                         'face 'minibuffer-prompt))))
-
-(defun isearch-forward-filename* (&optional arg)
-  "Do incremental filename backward."
-  (interactive "P")
-  (if (mark-filename@)
-      (if arg
-          (isearch-backward*)
-        (isearch-forward*))
-    (comment arg)
-    (message "%s: [No filename at point]"
-             (propertize "Filename I-search"
-                         'face 'minibuffer-prompt))))
+  (isearch-forward* style t))
 
 
 (with-eval-after-load 'isearch
-  (define-key% (current-global-map) (kbd "C-s") #'isearch-forward*1)
-  (define-key% (current-global-map) (kbd "C-r") #'isearch-backward*1)
-  (define-key% (current-global-map)
-    (kbd "M-s s") #'isearch-forward-symbol*)
-  (define-key% (current-global-map)
-    (kbd "M-s w") #'isearch-forward-word*)
-  (define-key% (current-global-map)
-    (kbd "M-s q") #'isearch-forward-string*)
-  (define-key% (current-global-map)
-    (kbd "M-s f") #'isearch-forward-filename*))
+  (define-key% (current-global-map) (kbd "C-s") #'isearch-forward*)
+  (define-key% (current-global-map) (kbd "C-r") #'isearch-backward*))
 
 
  ;; end of file
