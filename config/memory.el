e7b4e9f001be87c08df5c15ea0da5e7cb77b0d06
南山竹 <junjiemars@gmail.com>
2020-08-31 22:42:59 +0800

    docstring for memory.el

--- config/memory.el
+++ config/memory.el
@@ -71,7 +71,7 @@
 
 ;; Read desktop
 (defun self-desktop-read! ()
-  
+  "Read the desktop of the previous Emacs instance."
   (unless-graphic%
     (when-version% <= 24.4
       (when-version% > 25
@@ -95,13 +95,14 @@
 
 ;; Save desktop
 (defun self-desktop-save! ()
+  "Save the desktop of the current Emacs instance."
   (when (self-spec->*env-spec :desktop :allowed)
     (let ((f (self-spec->*env-spec :desktop :files-not-to-save)))
       (when f (setq% desktop-files-not-to-save f 'desktop)))
-    
+
     (let ((b (self-spec->*env-spec :desktop :buffers-not-to-save)))
       (when b (setq% desktop-buffers-not-to-save b 'desktop)))
-    
+
     (let ((m (self-spec->*env-spec :desktop :modes-not-to-save)))
       (setq% desktop-modes-not-to-save
              (append '(tags-table-mode) m) 'desktop))
