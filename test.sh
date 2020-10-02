3764b67a6cfbed98cadd260510269abbec79e5ee
南山竹 <junjiemars@gmail.com>
2020-10-03 00:29:57 +0800

    ci: refine test.sh

--- test.sh
+++ test.sh
@@ -1,20 +1,36 @@
 #!/usr/bin/env bash
 
-ROOT="`cd $(dirname $0) && pwd`"
-EMACS="${EMACS:-emacs}"
+_ROOT_="`cd $(dirname $0) && pwd`"
+_EMACS_="${EMACS:-emacs}"
+_TEST_="${_TEST_:-basic}"
 
-emacs --batch                                     \
-      --no-site-file                              \
-      --load="${ROOT}/init.el"                    \
-      --eval='(clean-compiled-files)'             \
-      --load="${ROOT}/init.el"
+test_basic() {
+  ${_EMACS_} --batch                            \
+             --no-site-file                     \
+             --load="${_ROOT_}/init.el"         \
+             --eval='(clean-compiled-files)'    \
+             --load="${_ROOT_}/init.el"
+}
 
-# emacs --batch                                   \
+test_debug() {
+  ${_EMACS_} --debug-init                       \
+             --no-site-file                     \
+             --load="${_ROOT_}/init.el"         \
+             --eval='(clean-compiled-files)'    \
+             --load="${_ROOT_}/init.el"
+}
+
+case "${_TEST_}" in
+  basic)    test_basic    ;;
+  debug)    test_debug    ;;
+esac
+
+# ${_EMACS_} --batch                              \
 #       --no-site-file                            \
-#       --load="${ROOT}/init.el"                  \
+#       --load="${_ROOT_}/init.el"                \
 #       --eval='(clean-compiled-files)'           \
-#       --load="${ROOT}/init.el"                  \
+#       --load="${_ROOT_}/init.el"                \
 #       --load="ert"                              \
-#       --load="${ROOT}/test.el"                  \
+#       --load="${_ROOT_}/test.el"                \
 #       --eval="(ert-run-tests-batch-and-exit)"
 
