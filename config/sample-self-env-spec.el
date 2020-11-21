e3077f875e8b53b93948741d6e3a60c91d18dd10
南山竹 <junjiemars@gmail.com>
2020-11-21 21:36:06 +0800

    refine readme

--- config/sample-self-env-spec.el
+++ config/sample-self-env-spec.el
@@ -23,103 +23,103 @@
 
 ;;; theme
 (*self-env-spec*
- :put
- :theme (list :name nil ;; 'dracula
-              :custom-theme-directory (emacs-home* "theme/")
-              :compile nil ;; expert option
-              :allowed nil))
+ :put :theme
+ (list :name nil ;; 'dracula
+       :custom-theme-directory (emacs-home* "theme/")
+       :compile nil ;; expert option
+       :allowed nil))
 
 ;;; frame
 (*self-env-spec*
- :put
- :frame (list :initial `((fullscreen . fullheight))
-              :default `((font . ,(if-platform% 'darwin
-                                      "Monaco-17"
-                                    (if-platform% 'windows-nt
-                                        "Consolas-13"
-                                      "DejaVu Sans Mono-14"))))
-              :frame-resize-pixelwise t
-              :allowed nil))
+ :put :frame
+ (list :initial `((fullscreen . fullheight))
+       :default `((font . ,(if-platform% 'darwin
+                               "Monaco-17"
+                             (if-platform% 'windows-nt
+                                 "Consolas-13"
+                               "DejaVu Sans Mono-14"))))
+       :frame-resize-pixelwise t
+       :allowed nil))
 
 ;;; glyph
 (*self-env-spec*
- :put
- :glyph `((:name ,(if-platform% 'darwin
-                      "Hack"
-                    (if-platform% 'windows-nt
-                        "Courier New"
-                      "DejaVu Sans Mono"))
-                 :size 17
-                 :scale nil
-                 :scripts (greek)
-                 :allowed t)
-          (:name ,(if-platform% 'darwin
-                      "PingFang"
-                    (if-platform% 'windows-nt
-                        "Microsoft YaHei"
-                      "Noto Sans"))
-                 :size 12
-                 :scale nil
-                 :scripts (han)
-                 :allowed nil)))
+ :put :glyph
+ `((:name ,(if-platform% 'darwin
+               "Hack"
+             (if-platform% 'windows-nt
+                 "Courier New"
+               "DejaVu Sans Mono"))
+          :size 17
+          :scale nil
+          :scripts (greek)
+          :allowed t)
+   (:name ,(if-platform% 'darwin
+               "PingFang"
+             (if-platform% 'windows-nt
+                 "Microsoft YaHei"
+               "Noto Sans"))
+          :size 12
+          :scale nil
+          :scripts (han)
+          :allowed nil)))
 
 
 ;;; shell
 (*self-env-spec*
- :put
- :shell (list :copy-vars `("PATH")
-              :spin-vars nil                 ;; `(("ZZZ" . "123"))
-              :options '("-i" "2>/dev/null") ;; '("--login")
-              :exec-path t
-              :shell-file-name (executable-find% "bash")
-              :prompt (list :zsh "%n@%m:%~ %# "
-                            :bash "\\u@\\h:\\w\\$ ")
-              :allowed nil))
+ :put :shell
+ (list :copy-vars `("PATH")
+       :spin-vars nil                        ;; `(("ZZZ" . "123"))
+       :options '("-i" "2>/dev/null")        ;; '("--login")
+       :exec-path t
+       :shell-file-name (executable-find% "bash")
+       :prompt (list :zsh "%n@%m:%~ %# "
+                     :bash "\\u@\\h:\\w\\$ ")
+       :allowed nil))
 
 ;;; desktop
 (*self-env-spec*
- :put
- :desktop (list :files-not-to-save
-                "\.t?gz$\\|\.zip$\\|\.desktop\\|~$\\|^/sudo:\\|^/ssh[x]?:\\|\.elc$"
-                :buffers-not-to-save "^TAGS\\|\\.log"
-                :modes-not-to-save
-                '(dired-mode fundamental-mode eww-mode rmail-mode)
-                :restore-eager 8
-                :allowed nil))
+ :put :desktop
+ (list :files-not-to-save
+       "\.t?gz$\\|\.zip$\\|\.desktop\\|~$\\|^/sudo:\\|^/ssh[x]?:\\|\.elc$"
+       :buffers-not-to-save "^TAGS\\|\\.log"
+       :modes-not-to-save
+       '(dired-mode fundamental-mode eww-mode rmail-mode)
+       :restore-eager 8
+       :allowed nil))
 
 ;;; eshell
 (*self-env-spec*
- :put
- :eshell (list :visual-commands '("mtr")
-               :destroy-buffer-when-process-dies nil
-               :visual-subcommands nil ;; '(("git" "log"))
-               :visual-options nil
-               :allowed nil))
+ :put :eshell
+ (list :visual-commands '("mtr")
+       :destroy-buffer-when-process-dies nil
+       :visual-subcommands nil ;; '(("git" "log"))
+       :visual-options nil
+       :allowed nil))
 
 ;;; socks
 (*self-env-spec*
- :put
- :socks (list :port 32000
-              :server "127.0.0.1"
-              :version 5
-              :allowed nil))
+ :put :socks
+ (list :port 32000
+       :server "127.0.0.1"
+       :version 5
+       :allowed nil))
 
 (*self-env-spec*
- :put
- :package (list :remove-unused nil
-                :package-check-signature 'allow-unsigned
-                :allowed nil))
+ :put :package
+ (list :remove-unused nil
+       :package-check-signature 'allow-unsigned
+       :allowed nil))
 
 ;;; edit
 (*self-env-spec*
- :put
- :edit (list :tab-width 2
-             :standard-indent 2
-             :auto-save-default nil
-             :disable-indent-tabs-mode '(c-mode-common-hook
-                                         sh-mode-hook
-                                         emacs-lisp-mode-hook)
-             :narrow-to-region nil
-             :allowed t)) 
+ :put :edit
+ (list :tab-width 2
+       :standard-indent 2
+       :auto-save-default nil
+       :disable-indent-tabs-mode '(c-mode-common-hook
+                                   sh-mode-hook
+                                   emacs-lisp-mode-hook)
+       :narrow-to-region nil
+       :allowed t)) 
 
 ;;; eof
