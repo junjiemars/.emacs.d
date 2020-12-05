614601b884e082edf79aba7fc6e91c6990590d97
南山竹 <junjiemars@gmail.com>
2020-12-05 13:12:03 +0800

    axiom testing

--- test.sh
+++ test.sh
@@ -3,24 +3,38 @@
 _ROOT_="`cd $(dirname $0) && pwd`"
 _EMACS_="${EMACS:-emacs}"
 _TEST_="${_TEST_:-bone}"
+_ENV_VER_=
+_ENV_ERT_=
+
+echo_env() {
+  echo "------------"
+  echo "VERSION: $_ENV_VER_"
+  echo "ERT: $_ENV_ERT_"
+  echo "TEST: $1"
+  echo "------------"
+}
 
 test_bone() {
+  echo_env "bone"
   ${_EMACS_} --batch                              \
              --no-site-file                       \
              --load="${_ROOT_}/init.el"
 }
 
 test_debug() {
+  echo_env "debug"
   ${_EMACS_} --debug-init                         \
              --no-site-file                       \
              --load="${_ROOT_}/init.el"
 }
 
 test_axiom() {
-  local has_ert=`${_EMACS_} --batch --eval="(prin1 (require 'ert nil t))"`
-  if [ "ert" = "$has_ert" ]; then
+  echo_env "axiom"
+  if [ "ert" = "$_ENV_ERT_" ]; then
     ${_EMACS_} --batch                                  \
                --no-site-file                           \
+               --load="${_ROOT_}/init.el"               \
+               --eval="(clean-compiled-files)"          \
                --load="${_ROOT_}/init.el"               \
                --load="ert"                             \
                --load="${_ROOT_}/test.el"               \
@@ -29,6 +43,12 @@ test_axiom() {
     echo "#skipped axiom testing, ert no found"
   fi
 }
+
+# check env
+_ENV_VER_=`${_EMACS_} --batch --eval="(prin1 emacs-version)"`
+if [ ert = `${_EMACS_} --batch --eval="(prin1 (require 'ert nil t))"` ]; then
+  _ENV_ERT_="ert"
+fi
 
 case "${_TEST_}" in
   bone)     test_bone     ;;
