4524f29a928b0af2a2f38992bb2a786d468dd0bb
南山竹 <junjiemars@gmail.com>
2021-05-23 11:51:07 +0800

    node: indent

--- config/node.el
+++ config/node.el
@@ -67,20 +67,21 @@
 
 
 (defconst +node-emacs-module+
-  "function node_emacs_apropos(what, max) {
-		global.repl.repl.complete(what, (err, data) => {
-				if (err) {
-						console.log('()');
-				} else {
-						if (Array.isArray(data)) {
+  "
+function node_emacs_apropos(what, max) {
+    global.repl.repl.complete(what, (err, data) => {
+        if (err) {
+            console.log('()');
+        } else {
+            if (Array.isArray(data)) {
                 let out = [];
                 for (let ss of data[0]) {
                     out.push('\"' + ss + '\"');
                 }
-								console.log('(%s)', out.join(' '));
-						}
-				}
-		});
+                console.log('(%s)', out.join(' '));
+            }
+        }
+    });
 }
 "
   "The module of node-emacs.")
