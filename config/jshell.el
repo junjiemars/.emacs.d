1f05f0111ba5989a07437b2aaf3f298e992d07e6
南山竹 <junjiemars@gmail.com>
2021-06-19 19:10:26 +0800

    jshell: startup file

--- config/jshell.el
+++ config/jshell.el
@@ -54,17 +54,6 @@
 
 (defconst +jshell-emacs-module+
   "
-import java.io.*;
-import java.math.*;
-import java.net.*;
-import java.nio.file.*;
-import java.util.*;
-import java.util.concurrent.*;
-import java.util.function.*;
-import java.util.prefs.*;
-import java.util.regex.*;
-import java.util.stream.*;
-
 void jshell_emacs_apropos(String what, int max) {
   System.out.println(what);
 }
