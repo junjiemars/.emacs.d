d4da98796baed5f4b86c52e146013fffbcd963b7
南山竹 <junjie.mars@icloud.com>
2021-08-13 18:29:02 +0800

    js-indent-level follow :tab-width

--- config/on-js-autoload.el
+++ config/on-js-autoload.el
@@ -11,7 +11,13 @@
 (with-eval-after-load 'js
 
   (when-var% js-js-tmpdir 'js
-    (setq% js-js-tmpdir (v-home! ".js/") 'js)))
+    (setq% js-js-tmpdir (v-home! ".js/") 'js))
+
+  (when-var% js-indent-level 'js
+    (when (*self-env-spec* :get :edit :allowed)
+      (setq% js-indent-level
+             (*self-env-spec* :get :edit :tab-width)
+             'js))))
 
 
 ;;; eof
