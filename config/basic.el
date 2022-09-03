142573180c6e67b7b917f90afa84b6ea80cbe38b
南山竹 <junjie.mars@icloud.com>
2022-09-03 17:41:10 +0800

    abbrev

--- config/basic.el
+++ config/basic.el
@@ -17,15 +17,16 @@
 
 ;; Versioned Dirs: .*
 
-;; Auto-save
+;; auto-save
 (setq auto-save-list-file-prefix (v-home! ".save/auto-"))
+
 
 ;; Emacs can automatically create backup files. This tells Emacs to
 ;; put all backups in ~/.emacs.d/backups. More info:
 ;; http://www.gnu.org/software/emacs/manual/html_node/elisp/Backup-Files.html
 (setq backup-directory-alist `(("." . ,(v-home! ".backup/"))))
 
-;; calc
+;; `calc'
 (setq% calc-settings-file (v-home! ".calc/calc.el") 'calc)
 
 ;; eww bookmarks
@@ -33,55 +34,60 @@
     <= 24.4
   (setq% eww-bookmarks-directory (v-home! ".bookmarks/") 'eww))
 
-;; Bookmark: file in which to save bookmarks
+;; `bookmark': file in which to save bookmarks
 (setq% bookmark-default-file
        (v-home! ".bookmarks/emacs.bmk") 'bookmark)
 
-;; Eshell
+;; `eshell'
 (setq% eshell-directory-name (v-home! ".eshell/") 'eshell)
 
-;; Games: a directory for game scores
+;; `gamegrid': a directory for game scores
 (setq% gamegrid-user-score-file-directory (v-home! ".games/") 'gamegrid)
 
-;; Ido saved state between invocations
+;; `ido' saved state between invocations
 (setq% ido-save-directory-list-file (v-home! ".ido/ido.last") 'ido)
 
-;; Image dired: where thumbnail images are stored.
+;; `image-dired': where thumbnail images are stored.
 (setq% image-dired-dir (v-home! ".dired/image/") 'image-dired)
 
 ;; Savehist: save minibuffer history
 (setq% savehist-file (v-home! ".minibuffer/history") 'savehist)
 
-;; Recentf: save the recent list into
+;; `recentf': save the recent list into
 (setq% recentf-save-file (v-home! ".recentf/recentf") 'recentf)
 
-;; Rmail
+;; `rmail'
 (setq rmail-file-name (v-home! ".mail/RMAIL"))
 
-;; Nsm: Network Security Manager
+;; `nsm': Network Security Manager
 (setq% nsm-settings-file (v-home! ".nsm/security.data") 'nsm)
 
+;; `saveplace'
 ;; When you visit a file, point goes to the last place where it
 ;; was when you previously visited the same file.
 ;; http://www.emacswiki.org/emacs/SavePlace
 (setq% save-place-file (v-home! ".places/places") 'saveplace)
 
-;; Server
+;; `server'
 (setq% server-auth-dir (v-home! ".server/") 'server)
 
-;; Semantic
+;; `semantic'
 (when-version% <= 23
   (setq% semanticdb-default-save-directory
          (v-home! ".semantic/db/") 'semantic/db-file))
 
-;; Tramp
+;; `tramp'
 (when-version% <= 23
   (setq% tramp-persistency-file-name (v-home! ".tramp/tramp")
          (if-version% > 24 'tramp
            'tramp-cache)))
 
-;; Url
+;; `url'
 (setq% url-configuration-directory (v-home! ".url/") 'url)
+
+
+;; `abbrev'
+(setq% abbrev-file-name (v-home! ".abbrev/defs") 'abbrev)
 
 
  ;; Versioned Dirs
