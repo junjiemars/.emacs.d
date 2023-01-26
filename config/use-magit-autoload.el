43000cf5b96ea5b110d996aa753b691a4abbd79d
南山竹 <junjie.mars@icloud.com>
2023-01-26 12:58:50 +0800

    magit transient directory

--- config/use-magit-autoload.el
+++ config/use-magit-autoload.el
@@ -7,7 +7,7 @@
 (with-eval-after-load 'magit
 
   (when-platform% 'windows-nt
-    
+
     (when (executable-find% "git" t)
       ;; On Windows try to open remote git repo via sshx
       ;; will trigger `magit' error: No such file or directory.
@@ -15,9 +15,21 @@
       (setq% magit-git-executable "git" 'magit))))
 
 
+
+(defmacro-if-feature% transient)
+
+(if-feature-transient%
+    (with-eval-after-load 'transient
+      (let ((dir (v-home! ".transient/")))
+        (setq transient-history-file (concat dir "history.el")
+              transient-levels-file (concat dir "levels.el")
+              transient-values-file (concat dir "values.el")))))
+
+
 (when-fn% 'magit-status 'magit
   ;; should take up the `vc-create-tag' key binding
   (define-key (current-global-map) (kbd "C-x v M") #'magit-status))
+
 
 
 ;; end of file
