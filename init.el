863633ed2b740e8df1d94e147b6710ffeab5abe0
南山竹 <junjiemars@gmail.com>
2023-01-29 14:54:20 +0800

    refine v-home% macro

--- init.el
+++ init.el
@@ -111,7 +111,7 @@ The FILE should be posix path, see `path-separator'."
 
 
 (defmacro v-home% (file)
-  "Return versioned path of `emacs-home'/`v-path*'/FILE."
+  "Return versioned path of `emacs-home'/`v-path*'/FILE at compile-time."
   (let ((_vf_ (gensym*)))
     (let ((_vf_ (v-home* file)))
       `,_vf_)))
