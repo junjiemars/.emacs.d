28f512a95410353efa1f0532bfc7cf8669d99d59
junjie <junjie.mars@icloud.com>
2023-12-06 20:42:00 +0800

    eglot*-shutdown-all

--- config/on-eglot-autoload.el
+++ config/on-eglot-autoload.el
@@ -114,7 +114,8 @@ and INDENT."
 
  (defun eglot*-shutdown-all ()
    (condition-case _
-       (let ((debug-on-error nil))
+       (let ((debug-on-error nil)
+             (debug-on-quit nil))
          (prog1 t
            (eglot-shutdown-all)))
      (error t))))
