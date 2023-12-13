333e42fcca844960f5f2d0005eebe273c3145f6c
junjie <junjiemars@gmail.com>
2023-12-13 17:06:50 +0800

    clean

--- config/on-js-autoload.el
+++ config/on-js-autoload.el
@@ -11,13 +11,7 @@
 (with-eval-after-load 'js
 
   (when-version% > 28.1
-    (setq% js-js-tmpdir (v-home! ".js/") 'js))
-
-  (when-var% js-indent-level 'js
-    (when (*self-env-spec* :get :edit :allowed)
-      (setq% js-indent-level
-             (*self-env-spec* :get :edit :tab-width)
-             'js))))
+    (setq% js-js-tmpdir (v-home! ".js/") 'js)))
 
 
 ;;; eof
