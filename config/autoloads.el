8496b5699aa3d12862232f762b13e426162d6cd3
junjie <junjiemars@gmail.com>
2024-01-08 09:08:17 +0800

    if-feature-*%: for ancient Emacs

--- config/autoloads.el
+++ config/autoloads.el
@@ -12,28 +12,32 @@
 (defmacro when-feature-eww% (&rest body)
   (declare (indent 0))
   (if-feature-eww%
-      `(progn% ,@body)))
+      `(progn% ,@body)
+    `(comment ,@body)))
 
 ;; `project' builtin since Emacs-26+
 (defmacro-if-feature% project)
 (defmacro when-feature-project% (&rest body)
   (declare (indent 0))
   (if-feature-project%
-      `(progn% ,@body)))
+      `(progn% ,@body)
+    `(comment ,@body)))
 
 ;; `eglot' builtin since Emacs-29+
 (defmacro-if-feature% eglot)
 (defmacro when-feature-eglot% (&rest body)
   (declare (indent 0))
   (if-feature-eglot%
-      `(progn% ,@body)))
+      `(progn% ,@body)
+    `(comment ,@body)))
 
 ;; `treesit': builtin since Emacs-29+
 (defmacro-if-feature% treesit)
 (defmacro when-feature-treesit% (&rest body)
   (declare (indent 0))
   (if-feature-treesit%
-      `(progn% ,@body)))
+      `(progn% ,@body)
+    `(comment ,@body)))
 
 ;; (defalias '*org-babel-schemes*
 ;;   (lexical-let% ((i '()))
