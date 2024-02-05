9c2a775f4cfebe27e200afb6c4bf78fd872e3055
junjie <junjiemars@gmail.com>
2024-02-05 13:35:25 +0800

    self-edit-env->disable-indent-tabs-mode

--- config/edit.el
+++ config/edit.el
@@ -35,10 +35,8 @@
 
     ;; disable `indent-tabs-mode'
     (dolist* (m (edit-spec->* :disable-indent-tabs-mode))
-      (let ((h (intern-soft (format "%s-hook" m))))
-        (when h
-          (add-hook
-           h #'self-edit-env->disable-indent-tabs-mode))))
+      (let ((h (intern (format "%s-hook" m))))
+        (add-hook h #'self-edit-env->disable-indent-tabs-mode)))
 
     ;; default `tab-width' and `standard-indent'
     (setq-default tab-width (edit-spec->* :tab-width)
