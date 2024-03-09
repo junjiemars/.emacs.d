ece63a505e755bbad874f5e9318d21c2b63c6821
南山竹 <junjie.mars@icloud.com>
2024-03-09 14:58:43 +0800

    avoid `ls does not support --dired'

--- config/on-dired-autoload.el
+++ config/on-dired-autoload.el
@@ -14,7 +14,6 @@
 (autoload 'on-dired-aux-init! (v-home%> "config/direds"))
 (autoload 'on-arc-mode-init! (v-home%> "config/direds"))
 
-
 ;;; `dired' after load
 (with-eval-after-load 'dired
   (make-thread* #'on-dired-init!))
@@ -26,6 +25,25 @@
 ;;; `arc-mode' after load
 (with-eval-after-load 'arc-mode
   (make-thread* #'on-arc-mode-init!))
+
+;; prefer GNU's ls (--dired option) on Windows or Darwin. on
+;; Windows: `dired-mode' does not display executable flag in file
+;; mode，see `dired-use-ls-dired' and `ido-dired' for more defails
+(when% (executable-find%
+        "ls"
+        (lambda (bin)
+          (let ((home (shell-command* bin (emacs-home*))))
+            (zerop (car home)))))
+  ;; on Drawin: the builtin ls does not support --dired option
+  (setq% dired-use-ls-dired
+         (executable-find%
+          "ls"
+          (lambda (bin)
+            (let ((dired (shell-command* bin "--dired")))
+              (zerop (car dired)))))
+         'dired)
+  ;; using `insert-directory-program'
+  (setq% ls-lisp-use-insert-directory-program t 'ls-lisp))
 
 ;;; autoload
 (autoload 'dired-get-file-for-visit "dired")
