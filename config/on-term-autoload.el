c57675c924d58cbb2a9c45524bd7bafbd9894097
junjie <junjie.mars@icloud.com>
2024-04-08 20:28:15 +0800

    merge ielm, eshell, term into one

--- config/on-term-autoload.el
+++ config/on-term-autoload.el
@@ -7,10 +7,12 @@
 ;;;;
 
 (declare-function on-term-init! (v-home%> "config/terms"))
-(declare-function on-eshell-init! (v-home%> "config/eshells"))
 (declare-function term*-unify-shell-prompt (v-home%> "config/terms"))
+(declare-function on-eshell-init! (v-home%> "config/eshells"))
+(declare-function on-ielm-init! (v-home%> "config/elisps"))
 (autoload 'on-term-init! (v-home%> "config/terms"))
 (autoload 'on-eshell-init! (v-home%> "config/eshells"))
+(autoload 'on-ielm-init! (v-home%> "config/elisps"))
 
 ;;; `term'
 (with-eval-after-load 'term
@@ -19,6 +21,10 @@
 ;;; `eshell'
 (with-eval-after-load 'eshell
   (make-thread* #'on-eshell-init!))
+
+;;; `ielm' after load
+(with-eval-after-load 'ielm
+  (make-thread* #'on-ielm-init!))
 
 ;;; autoload
 (autoload 'term*-unify-shell-prompt (v-home%> "config/terms")
