ee27ea475f1fb4b23f8f7098dd0690e6bc681b6b
junjie <junjie.mars@icloud.com>
2024-04-13 08:13:16 +0800

    remove basic.el

--- config/basic.el
+++
@@ -1,12 +0,0 @@
-;; -*- lexical-binding:t -*-
-;;;;
-;; Nore Emacs
-;; https://github.com/junjiemars/.emacs.d
-;;;;
-;; basic.el
-;;;;
-;; Commentary: functional facilities.
-;;;;
-
-
-;; end of basic.el
