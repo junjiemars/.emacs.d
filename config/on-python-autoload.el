8de445c397eec7c503cd0903f6aae25f133602a5
南山竹 <junjie.mars@icloud.com>
2024-04-29 22:59:45 +0800

    clean

--- config/on-python-autoload.el
+++ config/on-python-autoload.el
@@ -8,17 +8,15 @@
 
 (declare-function on-python-init! (v-home%> "config/pythons"))
 (declare-function python*-program (v-home%> "config/pythons"))
-(declare-function python*-venv! (v-home%> "config/pythons"))
+(declare-function python*-venv-activate! (v-home%> "config/pythons"))
 (autoload 'on-python-init! (v-home%> "config/pythons"))
+(autoload 'python*-program (v-home%> "config/pythons")
+  "The program of python executable.")
+(autoload 'python*-venv-activate! (v-home%> "config/pythons")
+  "Activate python\\='s venv.")
 
 ;;; `python' after load
 (with-eval-after-load 'python
   (make-thread* #'on-python-init!))
-
-;;; autoload
-(autoload 'python*-program (v-home%> "config/pythons")
-  "The program of python executable.")
-(autoload 'python*-venv-activate! (v-home%> "config/pythons")
-  "Activate python\\='s venv." t)
 
 ;; end of on-python-autoload.el
