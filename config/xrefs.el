5044044cd43d0642c24706fbdc49f42a406b83f5
南山竹 <junjie.mars@icloud.com>
2024-07-14 21:22:34 +0800

    advice xref--show-location-after

--- config/xrefs.el
+++ config/xrefs.el
@@ -26,6 +26,18 @@
 
 ;; end of `xref-find-definitions' macro
 
+;;; `xref--show-location' macro
+
+(defmacro-if-fn% xref--show-location xref)
+
+(defmacro when-fn-xref--show-location% (&rest body)
+  (declare (indent 0))
+  (if-fn-xref--show-location%
+      `(progn% ,@body)
+    `(comment ,@body)))
+
+;; end of `xref--show-location' macro
+
 ;;;
 ;; go into `view-mode'
 ;; `xref-find-definitions' into `view-mode'
@@ -46,17 +58,24 @@
             ((eq op :path) b)
             (t c)))))
 
-(defun xref*-find-definitions ()
-  (with-current-buffer (current-buffer)
-    (when (file-in-dirs-p (buffer-file-name (current-buffer))
-                          (xref*-read-only-dirs))
-      (view-mode 1))))
+(defun xref*-buffer-in-view-mode (&optional buffer)
+  (let* ((buf (or buffer (current-buffer)))
+         (name (buffer-file-name buf)))
+    (with-current-buffer buf
+      (when (and name (file-in-dirs-p name (xref*-read-only-dirs)))
+        (view-mode 1)))))
 
 (when-fn-xref-find-definitions%
   ;; `xref-find-definitions' into `view-mode'
   (defadvice xref-find-definitions
       (after xref-find-definitions-after first compile disable)
-    (xref*-find-definitions)))
+    (xref*-buffer-in-view-mode)))
+
+(when-fn-xref--show-location%
+ ;; `xref--show-location' into `view-mode'
+ (defadvice xref--show-location
+     (after xref--show-location-after first compile disable)
+   (xref*-buffer-in-view-mode (window-buffer ad-return-value))))
 
 (defun on-xref-init! ()
   (when (file-exists-p (xref*-read-only-dirs :path))
@@ -64,7 +83,11 @@
   (when-fn-xref-find-definitions%
     (ad-enable-advice #'xref-find-definitions 'after
                       "xref-find-definitions-after")
-    (ad-activate #'xref-find-definitions t)))
+    (ad-activate #'xref-find-definitions t))
+  (when-fn-xref--show-location%
+   (ad-enable-advice #'xref--show-location 'after
+                     "xref--show-location-after")
+   (ad-activate #'xref--show-location t)))
 
 
 (unless-fn% 'xref-find-references 'xref
@@ -89,7 +112,7 @@
 ;;; `find-tag' into `view-mode'
 (unless-fn-xref-find-definitions%
   (defadvice find-tag (after find-tag-after first compile disable)
-    (xref*-find-definitions)))
+    (xref*-buffer-in-view-mode)))
 
 (defun on-etags-init! ()
   "On \\=`etags\\=' initialization."
