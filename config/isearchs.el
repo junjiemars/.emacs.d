5570da9b247be0102b2dac459ecec090a0d5d573
junjie <junjie.mars@icloud.com>
2024-09-07 18:34:48 +0800

    clean: autoload for marks

--- config/isearchs.el
+++ config/isearchs.el
@@ -19,11 +19,12 @@
 (declare-function _mark_symbol@_ (v-home%> "config/marks"))
 (declare-function _mark_thing_ (v-home%> "config/marks"))
 (declare-function _mark_word@_ (v-home%> "config/marks"))
-(autoload '_mark_filename@_ (v-home%> "config/marks") nil nil 'macro)
-(autoload '_mark_quoted_symmetry@_ (v-home%> "config/marks") nil nil 'macro)
-(autoload '_mark_symbol@_ (v-home%> "config/marks") nil nil 'macro)
-(autoload '_mark_thing_ (v-home%> "config/marks") nil nil 'macro)
-(autoload '_mark_word@_ (v-home%> "config/marks") nil nil 'macro)
+(eval-when-compile (require 'marks (v-home%> "config/marks")))
+;; (autoload '_mark_filename@_ (v-home%> "config/marks") nil nil 'macro)
+;; (autoload '_mark_quoted_symmetry@_ (v-home%> "config/marks") nil nil 'macro)
+;; (autoload '_mark_symbol@_ (v-home%> "config/marks") nil nil 'macro)
+;; (autoload '_mark_thing_ (v-home%> "config/marks") nil nil 'macro)
+;; (autoload '_mark_word@_ (v-home%> "config/marks") nil nil 'macro)
 
 
 ;; end of macro
