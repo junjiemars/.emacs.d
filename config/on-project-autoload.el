b49a5c361f9b35ffbd38e43fdf0bbfdc82fbc90b
junjie <junjie.mars@icloud.com>
2024-11-03 22:15:55 +0800

    clean

--- config/on-project-autoload.el
+++ config/on-project-autoload.el
@@ -7,16 +7,13 @@
 ;;;;
 
 (autoload 'on-project-init! (v-home%> "config/projects"))
+(autoload 'project*-root-dirs (v-home%> "config/projects"))
 
 ;;; `project' after load
 (with-eval-after-load 'project
   (make-thread* #'on-project-init!))
 
 (when-fn% 'project-find-file 'project
-  (define-key% (current-global-map) (kbd% "C-x p f")
-               #'project-find-file))
-
-;;; autoload
-(autoload 'project*-root-dirs (v-home%> "config/projects"))
+  (define-key% (current-global-map) (kbd% "c-x p f") #'project-find-file))
 
 ;; end of on-project-autoload.el
