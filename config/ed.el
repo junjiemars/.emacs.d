b3ae871663934df6f900a4a7a0a669573727744f
junjie <junjie.mars@icloud.com>
2024-11-08 23:42:28 +0800

    take on a time machine?

--- config/ed.el
+++ config/ed.el
@@ -44,7 +44,7 @@
               (rs (buffer-substring-no-properties (car bs) (cdr bs)))
               (f (save-str-to-file
                   rs
-                  (concat default-directory ,tmpfile)))
+                  (concat ,(path! (emacs-home* "scratch/")) ,tmpfile)))
               (ss (let ((x (shell-command* ,@shell* f)))
                     (and (= 0 (car x))
                          (read-str-from-file f)))))
