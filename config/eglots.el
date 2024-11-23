8a0143c4ac150678efd5f0e0139cc16d91e33e38
findidiot <junjie.mars@icloud.com>
2024-11-23 23:13:59 +0800

    refine eglot*-server-programs

--- config/eglots.el
+++ config/eglots.el
@@ -54,11 +54,9 @@
   (lexical-let%
       ((f (v-home% ".exec/eglot-server.el"))
        (b `((c-mode . ("clangd" "--header-insertion=never"))
-            ,(let ((cxx (executable-find% "c++")))
-               `(c++-mode
-                 .
-                 ("clangd"
-                  ,(format "--query-driver=%s" cxx)))))))
+            (c++-mode . ("clangd"
+                         ,(format "--query-driver=%s"
+                                  (executable-find% "c++")))))))
     (lambda (&optional op sexp)
       (cond ((eq op :read) (let ((s1 (read-sexp-from-file f)))
                              (dolist* (x s1 s1)
