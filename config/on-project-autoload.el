31cd88961bdb17103b56486f6d4fe6d61bf0a40b
findidiot <junjie.mars@icloud.com>
2024-12-20 11:20:02 +0800

    project-find-regexp key binding

--- config/on-project-autoload.el
+++ config/on-project-autoload.el
@@ -16,4 +16,7 @@
 (when-fn% project-find-file project
   (define-global-key% "pf" #'project-find-file))
 
+(when-fn% project-find-regexp project
+  (define-global-key% "pg" #'project-find-regexp))
+
 ;; end of on-project-autoload.el
