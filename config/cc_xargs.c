acec75e3f66bef0ee1fa5d118eff3f643f382c2a
findidiot <junjie.mars@icloud.com>
2025-04-01 00:36:46 +0800

    msvc

--- config/cc_xargs.c
+++ config/cc_xargs.c
@@ -1,13 +1,16 @@
-
 #include <stdio.h>
-int main(int argc, char **argv) {
+int
+main (int argc, char **argv)
+{
   int ch;
-  while (EOF != (ch = fgetc(stdin))) {
-    fputc(ch, stdout);
-  }
-  if (ferror(stdin)) {
-    perror(\"read failed from stdin\");
-    return 1;
-  }
+  while (EOF != (ch = fgetc (stdin)))
+    {
+      fputc (ch, stdout);
+    }
+  if (ferror (stdin))
+    {
+      perror ("read failed from stdin");
+      return 1;
+    }
   return 0;
 }
