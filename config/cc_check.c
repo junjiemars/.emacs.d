693f6b96de8564009253f98190be0eaf9935a3a5
findidiot <junjie.mars@icloud.com>
2025-03-31 20:04:43 +0800

    cc-spec->*

--- config/cc_check.c
+++ config/cc_check.c
@@ -1,8 +1,10 @@
 /* check `(cc*-cc)' is workable */
 /* required by `(emacs-home* "config/cc.el")' */
 /* from `(nore-emacs)' */
+#include <stdio.h>
 int
 main (void)
 {
+  printf ("Nore Emacs\n");
   return 0;
 }
