f29701250403f26c64fc6955cbf66cf29e2c0508
findidiot <junjie.mars@icloud.com>
2025-04-06 18:08:31 +0800

    avoid multiple transient versions problem

--- config/use-magit-autoload.el
+++ config/use-magit-autoload.el
@@ -11,6 +11,12 @@
 ;; toggle off `magit-auto-revert-mode' in `magit-autorevert'
 (set-default 'magit-auto-revert-mode nil)
 
+(eval-when-compile
+  (when-feature% transient
+    ;; avoid lower `transient-version' issue
+    (unload-feature 'transient t)
+    (require 'transient)))
+
 ;;; add `magit' into `vc*-frontend'
 (add-hook 'vc*-frontend-hook `("magit" . magit-status))
 
