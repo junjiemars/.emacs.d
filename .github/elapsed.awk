2e0dc522343bcd326d9a001f08e6cdf347680ebc
junjie <junjie.mars@icloud.com>
2025-04-24 20:55:24 +0800

    format

--- .github/elapsed.awk
+++ .github/elapsed.awk
@@ -10,15 +10,15 @@
 #
 
 function basename(path) {
-		sub(/.*\//, "", path)
-		return path
+    sub(/.*\//, "", path)
+    return path
 }
 
 /^[0-9]+\.[0-9]+/ {
-		filename = basename($14);
-		elapsed_real = $1 - $3;
-		printf "%10.6f %4d %10.6f %10.6f %3d %s\n",
-				$1, $2, $3, elapsed_real, NR, filename
+    filename = basename($14);
+    elapsed_real = $1 - $3;
+    printf "%10.6f %4d %10.6f %10.6f %3d %s\n",
+        $1, $2, $3, elapsed_real, NR, filename
 }
 
 # eof
--- .github/prof.sh
+++ .github/prof.sh
@@ -4,20 +4,20 @@ PROF_ROOT="$(cd -- $(dirname -- $0) && pwd)"
 PROF_FILE="$1"
 
 if [ -f "$PROF_FILE" ]; then
-	printf "# profile: elspsed time\n# ------------\n"
-	awk -f ${PROF_ROOT}/elapsed.awk "$PROF_FILE" \
-			| sort -k1nr -k2nr -k3nr
-	printf "# profile: elapsed real time\n# ------------\n"
-	awk -f ${PROF_ROOT}/elapsed.awk "$PROF_FILE" \
-		| sort -k4nr -k1nr -k2nr
-	printf "# profile: gc count\n# ------------\n"
-	awk -f ${PROF_ROOT}/elapsed.awk "$PROF_FILE" \
-		| sort -k2nr -k1nr -k3nr
-	printf "# profile: gc elapsed time\n# ------------\n"
-	awk -f ${PROF_ROOT}/elapsed.awk "$PROF_FILE" \
-		| sort -k3nr -k1nr -k2nr
-	printf "# profile: resource\n# ------------\n"
-	awk -f ${PROF_ROOT}/res.awk "$PROF_FILE"
+  printf "# profile: elspsed time\n# ------------\n"
+  awk -f ${PROF_ROOT}/elapsed.awk "$PROF_FILE" \
+      | sort -k1nr -k2nr -k3nr
+  printf "# profile: elapsed real time\n# ------------\n"
+  awk -f ${PROF_ROOT}/elapsed.awk "$PROF_FILE" \
+    | sort -k4nr -k1nr -k2nr
+  printf "# profile: gc count\n# ------------\n"
+  awk -f ${PROF_ROOT}/elapsed.awk "$PROF_FILE" \
+    | sort -k2nr -k1nr -k3nr
+  printf "# profile: gc elapsed time\n# ------------\n"
+  awk -f ${PROF_ROOT}/elapsed.awk "$PROF_FILE" \
+    | sort -k3nr -k1nr -k2nr
+  printf "# profile: resource\n# ------------\n"
+  awk -f ${PROF_ROOT}/res.awk "$PROF_FILE"
   printf "# profile: summary\n# ------------\n"
-	awk -f ${PROF_ROOT}/sum.awk "$PROF_FILE"
+  awk -f ${PROF_ROOT}/sum.awk "$PROF_FILE"
 fi
